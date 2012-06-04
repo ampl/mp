5f57d01ecdca0a989edd7b07c241ed8f2c457757
Victor Zverovich <victor.zverovich@gmail.com>
2012-06-04 12:03:57 -0700

    Fix a crash in IloException handling.

--- solvers/concert/main.cpp
+++ solvers/concert/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include "concert.h"
 #include "util.h"
 #include "solvers/asl.h"
@@ -7,11 +8,15 @@ using std::cerr;
 using std::endl;
 
 int main(int argc, char **argv) {
+  // Driver should be destroyed after any IloException is handled.
+  std::auto_ptr<Driver> d;
   try {
-    return Driver().run(argc, argv);
+    d.reset(new Driver());
+    return d->run(argc, argv);
   } catch (const IloException &e) {
     cerr << "Error: " << e << endl;
   } catch (const Error &e) {
     cerr << "Error: " << e.what() << endl;
   }
+  return 1;
 }
