37da0b2017495f74641989b0e81f432d0f0bdac7
Victor Zverovich <victor.zverovich@gmail.com>
2012-06-04 15:01:00 -0700

    Refactor option handling to get rid of remaining static objects that may change.

--- solvers/concert/concert.h
+++ solvers/concert/concert.h
@@ -1,10 +1,13 @@
 #ifndef AMPL_SOLVERS_CONCERT_H
 #define AMPL_SOLVERS_CONCERT_H
 
+#include <memory>
 #include <vector>
 #include <ilconcert/ilomodel.h>
 
 struct expr;
+struct keyword;
+struct Option_Info;
 
 extern int usenumberof;
 extern int debugexpr;
@@ -45,8 +48,15 @@ class Driver {
   IloEnv env_;
   IloModel mod_;
   IloNumVarArray vars_;
+  IloAlgorithm alg_;
   std::vector<NumberOf> numberofs_;
   std::vector<char> version_;
+  std::auto_ptr<Option_Info> oinfo_;
+  static keyword keywords_[];
+
+  // Do not implement.
+  Driver(const Driver&);
+  Driver& operator=(const Driver&);
 
   // Builds an array of expressions from the argument list of e.
   IloNumExprArray build_minmax_array(const expr *e);
@@ -57,14 +67,35 @@ class Driver {
   IloNumVar build_numberof(const expr *e);
 
  public:
+  // Options accessible from AMPL.
+  enum Option {
+    DEBUGEXPR,
+    ILOGOPTTYPE,
+    TIMING,
+    USENUMBEROF,
+    NUM_OPTIONS
+  };
+
+ private:
+  int options_[NUM_OPTIONS];
+
+  static char *set_option(Option_Info *oi, keyword *kw, char *value);
+  static char *set_option0(Option_Info *oi, keyword *kw, char *value);
+  static char *set_option1(Option_Info *oi, keyword *kw, char *value);
+
+ public:
   Driver();
   virtual ~Driver();
 
-  IloEnv get_env() const { return env_; }
+  IloEnv env() const { return env_; }
   IloModel mod() const { return mod_; }
+  IloAlgorithm alg() const { return alg_; }
 
   IloNumVarArray vars() const { return vars_; }
   void set_vars(IloNumVarArray vars) { vars_ = vars; }
+
+  int get_option(Option opt) const { return options_[opt]; }
+  void use_numberof(bool use = true) { options_[USENUMBEROF] = use; }
 
   // Walks an expression tree and constructs a function,
   // returning a Concert IloExpr.
