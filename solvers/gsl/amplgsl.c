7e5cba79c5ff2ed9b77689d48415393d930c4ea9
Victor Zverovich <victor.zverovich@gmail.com>
2012-06-25 14:10:22 -0700

    Wrap hypot3 and Airy functions.

--- solvers/gsl/amplgsl.c
+++ solvers/gsl/amplgsl.c
@@ -3,6 +3,7 @@
 #include <math.h>
 
 #include <gsl/gsl_math.h>
+#include <gsl/gsl_sf_airy.h>
 #include <gsl/gsl_sf_bessel.h>
 #include "solvers/funcadd.h"
 
@@ -44,7 +45,91 @@ static real amplgsl_hypot(arglist *al) {
   return hypot;
 }
 
-// TODO: gsl_hypot3, gsl_ldexp
+static real amplgsl_hypot3(arglist *al) {
+  real x = al->ra[0];
+  real y = al->ra[1];
+  real z = al->ra[2];
+  real hypot = gsl_hypot3(x, y, z);
+  if (al->derivs) {
+    real *derivs = al->derivs;
+    derivs[0] = x / hypot;
+    derivs[1] = y / hypot;
+    derivs[2] = z / hypot;
+    if (al->hes) {
+      real *hes = al->hes;
+      real dx2 = derivs[0] * derivs[0];
+      real dy2 = derivs[1] * derivs[1];
+      real dz2 = derivs[2] * derivs[2];
+      hes[0] =  (dy2 + dz2) / hypot;
+      hes[1] = -derivs[0] * derivs[1] / hypot;
+      hes[2] = -derivs[0] * derivs[2] / hypot;
+      hes[3] =  (dx2 + dz2) / hypot;
+      hes[4] = -derivs[1] * derivs[2] / hypot;
+      hes[5] =  (dx2 + dy2) / hypot;
+    }
+  }
+  return hypot;
+}
+
+static real amplgsl_sf_airy_Ai(arglist *al) {
+  real x = al->ra[0];
+  real value = gsl_sf_airy_Ai(x, GSL_PREC_DOUBLE);
+  if (al->derivs) {
+    *al->derivs = gsl_sf_airy_Ai_deriv(x, GSL_PREC_DOUBLE);
+    if (al->hes)
+      *al->hes = x * value;
+  }
+  return value;
+}
+
+static real amplgsl_sf_airy_Bi(arglist *al) {
+  real x = al->ra[0];
+  real value = gsl_sf_airy_Bi(x, GSL_PREC_DOUBLE);
+  if (al->derivs) {
+    *al->derivs = gsl_sf_airy_Bi_deriv(x, GSL_PREC_DOUBLE);
+    if (al->hes)
+      *al->hes = x * value;
+  }
+  return value;
+}
+
+static real amplgsl_sf_airy_Ai_scaled(arglist *al) {
+  real x = al->ra[0];
+  real value = gsl_sf_airy_Ai_scaled(x, GSL_PREC_DOUBLE);
+  if (al->derivs) {
+    if (x > 0) {
+      real sqrtx = sqrt(x);
+      *al->derivs = gsl_sf_airy_Ai_deriv_scaled(x, GSL_PREC_DOUBLE) +
+          sqrtx * gsl_sf_airy_Ai_scaled(x, GSL_PREC_DOUBLE);
+      if (al->hes)
+        *al->hes = (value + 4 * x * *al->derivs) / (2 * sqrtx);
+    } else {
+      *al->derivs = gsl_sf_airy_Ai_deriv(x, GSL_PREC_DOUBLE);
+      if (al->hes)
+        *al->hes = x * value;
+    }
+  }
+  return value;
+}
+
+static real amplgsl_sf_airy_Bi_scaled(arglist *al) {
+  real x = al->ra[0];
+  real value = gsl_sf_airy_Bi_scaled(x, GSL_PREC_DOUBLE);
+  if (al->derivs) {
+    if (x > 0) {
+      real sqrtx = sqrt(x);
+      *al->derivs = gsl_sf_airy_Bi_deriv_scaled(x, GSL_PREC_DOUBLE) -
+          sqrtx * gsl_sf_airy_Bi_scaled(x, GSL_PREC_DOUBLE);
+      if (al->hes)
+        *al->hes = -(value + 4 * x * *al->derivs) / (2 * sqrtx);
+    } else {
+      *al->derivs = gsl_sf_airy_Bi_deriv(x, GSL_PREC_DOUBLE);
+      if (al->hes)
+        *al->hes = x * value;
+    }
+  }
+  return value;
+}
 
 static real amplgsl_sf_bessel_J0(arglist *al) {
   real x = al->ra[0];
@@ -58,8 +143,19 @@ static real amplgsl_sf_bessel_J0(arglist *al) {
 }
 
 void funcadd_ASL(AmplExports *ae) {
+  // Elementary Functions
   addfunc("gsl_log1p", amplgsl_log1p, 0, 1, 0);
   addfunc("gsl_expm1", amplgsl_expm1, 0, 1, 0);
   addfunc("gsl_hypot", amplgsl_hypot, 0, 2, 0);
+  addfunc("gsl_hypot3", amplgsl_hypot3, 0, 3, 0);
+  // AMPL has built-in functions acosh, asinh and atanh so wrappers
+  // are not provided for their GSL equivalents.
+
+  // Airy Functions
+  addfunc("gsl_sf_airy_Ai", amplgsl_sf_airy_Ai, 0, 1, 0);
+  addfunc("gsl_sf_airy_Bi", amplgsl_sf_airy_Bi, 0, 1, 0);
+  addfunc("gsl_sf_airy_Ai_scaled", amplgsl_sf_airy_Ai_scaled, 0, 1, 0);
+  addfunc("gsl_sf_airy_Bi_scaled", amplgsl_sf_airy_Bi_scaled, 0, 1, 0);
+
   addfunc("gsl_sf_bessel_J0", amplgsl_sf_bessel_J0, 0, 1, 0);
 }
