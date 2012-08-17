f2d36a6f63873c45d3ccf00d7037e982e92227c8
Victor Zverovich <victor.zverovich@gmail.com>
2012-08-17 15:54:07 -0700

    Wrap digamma, trigamma and polygamma.

--- solvers/gsl/amplgsl.c
+++ solvers/gsl/amplgsl.c
@@ -2361,6 +2361,62 @@ static double amplgsl_sf_pow_int(arglist *al) {
   return check_result(al, gsl_sf_pow_int(x, n));
 }
 
+static double amplgsl_sf_psi_int(arglist *al) {
+  if (!check_int_arg(al, 0, "n"))
+    return 0;
+  return check_result(al, gsl_sf_psi_int((int)al->ra[0]));
+}
+
+static double amplgsl_sf_psi(arglist *al) {
+  double x = al->ra[0];
+  if (al->derivs) {
+    *al->derivs = x >= 0 || round(x) != x ? gsl_sf_psi_1(x) : GSL_NAN;
+    if (al->hes)
+      *al->hes = gsl_sf_psi_n(2, x);
+  }
+  return check_result(al, gsl_sf_psi(x));
+}
+
+static double amplgsl_sf_psi_1piy(arglist *al) {
+  double y = al->ra[0];
+  if (al->derivs) {
+    error(al, DERIVS_NOT_PROVIDED);
+    return 0;
+  }
+  return check_result(al, gsl_sf_psi_1piy(y));
+}
+
+static double amplgsl_sf_psi_1_int(arglist *al) {
+  if (!check_int_arg(al, 0, "n"))
+    return 0;
+  return check_result(al, gsl_sf_psi_1_int((int)al->ra[0]));
+}
+
+static double amplgsl_sf_psi_1(arglist *al) {
+  double x = al->ra[0];
+  if (al->derivs) {
+    *al->derivs = gsl_sf_psi_n(2, x);
+    if (al->hes)
+      *al->hes = gsl_sf_psi_n(3, x);
+  }
+  return check_result(al, gsl_sf_psi_1(x));
+}
+
+static double amplgsl_sf_psi_n(arglist *al) {
+  int n = 0;
+  double x = 0;
+  if (!check_int_arg(al, 0, "n"))
+    return 0;
+  n = (int)al->ra[0];
+  x = al->ra[1];
+  if (al->derivs) {
+    al->derivs[1] = x >= 0 || round(x) != x ? gsl_sf_psi_n(n + 1, x) : GSL_NAN;
+    if (al->hes)
+      al->hes[2] = gsl_sf_psi_n(n + 2, x);
+  }
+  return check_result(al, gsl_sf_psi_n(n, x));
+}
+
 #define ADDFUNC(name, num_args) \
     addfunc(#name, ampl##name, FUNCADD_REAL_VALUED, num_args, #name);
 
@@ -2644,5 +2700,17 @@ void funcadd_ASL(AmplExports *ae) {
 
   /* Power Function */
   ADDFUNC(gsl_sf_pow_int, 2);
+
+  /* Digamma Function */
+  ADDFUNC(gsl_sf_psi_int, 1);
+  ADDFUNC(gsl_sf_psi, 1);
+  ADDFUNC(gsl_sf_psi_1piy, 1);
+
+  /* Trigamma Function */
+  ADDFUNC(gsl_sf_psi_1_int, 1);
+  ADDFUNC(gsl_sf_psi_1, 1);
+
+  /* Polygamma Function */
+  ADDFUNC(gsl_sf_psi_n, 2);
   // TODO
 }
