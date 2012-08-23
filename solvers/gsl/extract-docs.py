ada50b87f13a8b3e574fe4843b81e23bb892e69e
Victor Zverovich <victor.zverovich@gmail.com>
2012-08-23 16:01:24 -0700

    Fix a misplaced parameter.

--- solvers/gsl/extract-docs.py
+++ solvers/gsl/extract-docs.py
@@ -7,7 +7,7 @@ with open('amplgsl.c', 'r+b') as input, open('index.rst', 'w') as output:
   map = mmap.mmap(input.fileno(), 0)
   for i in re.finditer(r'/\*\*(.*?)\*/', map, re.DOTALL):
     s = re.sub(r'\n +\* ?', r'\n', i.group(1))
-    s = re.sub(r'\$(.+?)\$', r':math:`\1`', s, re.DOTALL)
+    s = re.sub(r'\$(.+?)\$', r':math:`\1`', s, flags=re.DOTALL)
     m = re.search(r'@file (.*)', s)
     if m:
       filename = m.group(1)
