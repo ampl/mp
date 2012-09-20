e8aa2ebd5012cc4ca17a90368c2a0d97c015ddf8
Victor Zverovich <victor.zverovich@gmail.com>
2012-09-19 17:29:30 -0700

    Discover CPLEX libraries on MacOS X.

--- cmake/FindCPLEX.cmake
+++ cmake/FindCPLEX.cmake
@@ -30,7 +30,8 @@ if (NOT WIN32)
       /opt/ibm/ILOG /opt/IBM/ILOG $ENV{HOME}/ILOG $ENV{HOME}/ilog)
   set(CPLEX_LIB_PATH_SUFFIXES
     lib/x86_sles10_4.1/static_pic
-    lib/x86-64_sles10_4.1/static_pic)
+    lib/x86-64_sles10_4.1/static_pic
+    lib/x86-64_darwin9_gcc4.0/static_pic)
 else ()
   set(CPLEX_ILOG_DIRS C:/ILOG "C:/Program Files/IBM/ILOG")
   set(CPLEX_LIB_PATH_SUFFIXES
