65122c8783dde96a68520b569d7863a500ff0ab2
Victor Zverovich <victor.zverovich@gmail.com>
2012-10-04 17:12:44 -0700

    Fix a warning.

--- tests/ilogcp_test.cc
+++ tests/ilogcp_test.cc
@@ -74,7 +74,8 @@ struct ExprDeleter {
 void ExprDeleter::operator()(expr *e) const {
   if (!e) return;
   size_t op = reinterpret_cast<size_t>(e->op);
-  unsigned type = op != OPVARSUBVAR ? optype[op] : OPTYPE_UNARY;
+  OpType type = op != OPVARSUBVAR ?
+      static_cast<OpType>(optype[op]) : OPTYPE_UNARY;
   switch (type) {
   case OPTYPE_UNARY:
     (*this)(e->L.e);
