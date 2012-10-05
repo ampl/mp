a0f3a6789393f12b7a99b2771a717da6d7d3c184
Victor Zverovich <victor.zverovich@gmail.com>
2012-10-05 16:47:55 -0700

    Test if odbc table handler is registered.

--- tests/odbc_test.cc
+++ tests/odbc_test.cc
@@ -1,14 +1,36 @@
+/*
+ Tests of the AMPL ODBC table handler.
+
+ Copyright (C) 2012 AMPL Optimization LLC
+
+ Permission to use, copy, modify, and distribute this software and its
+ documentation for any purpose and without fee is hereby granted,
+ provided that the above copyright notice appear in all copies and that
+ both that the copyright notice and this permission notice and warranty
+ disclaimer appear in supporting documentation.
+
+ The author and AMPL Optimization LLC disclaim all warranties with
+ regard to this software, including all implied warranties of
+ merchantability and fitness.  In no event shall the author be liable
+ for any special, indirect or consequential damages or any damages
+ whatsoever resulting from loss of use, data or profits, whether in an
+ action of contract, negligence or other tortious action, arising out
+ of or in connection with the use or performance of this software.
+
+ Author: Victor Zverovich
+ */
+
 #include "gtest/gtest.h"
 #include "tests/function.h"
 #include "solvers/asl.h"
+#include "tests/config.h"
 
 namespace {
 
-class ODBCTest : public ::testing::Test {};
-
-TEST_F(ODBCTest, Load) {
+TEST(ODBCTest, Load) {
   fun::Library lib("../tables/ampltabl.dll");
   lib.Load();
-  // TODO: check if table handlers are registered
+  EXPECT_EQ("", lib.error());
+  EXPECT_TRUE(lib.GetHandler("odbc") != nullptr);
 }
 }
