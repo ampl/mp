bab14b90fadfbac6aa2bf4cd436802da18793186
Victor Zverovich <victor.zverovich@gmail.com>
2012-10-12 17:36:19 -0700

    Implement conversion of linear equality constraints and writing the solution in gecode.

--- solvers/gecode/gecode.h
+++ solvers/gecode/gecode.h
@@ -1,7 +1,27 @@
+#ifndef AMPL_SOLVERS_GECODE_H
+#define AMPL_SOLVERS_GECODE_H
+
+#include <memory>
+
+struct Option_Info;
+struct ASL_fg;
+
+// The Gecode driver for AMPL.
 class Driver {
+ private:
+  ASL_fg *asl;
+  std::auto_ptr<Option_Info> oinfo_;
+
+  // Do not implement.
+  Driver(const Driver&);
+  Driver& operator=(const Driver&);
+
  public:
+  Driver();
   virtual ~Driver();
 
-  void run(char **argv);
+  // Run the driver.
+  int run(char **argv);
 };
 
+#endif // AMPL_SOLVERS_GECODE_H
