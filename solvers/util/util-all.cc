9f2413cac01bc60866aedd941324420e03d2e68d
Victor Zverovich <victor.zverovich@gmail.com>
2012-10-19 10:24:03 -0700

    Include the util library source code in ilogcp to avoid link issues with different versions of runtime.

---
+++ solvers/util/util-all.cc
@@ -0,0 +1,24 @@
+/*
+ Source code of the whole util library as a single file.
+
+ Copyright (C) 2012 AMPL Optimization LLC
+
+ Permission to use, copy, modify, and distribute this software and its
+ documentation for any purpose and without fee is hereby granted,
+ provided that the above copyright notice appear in all copies and that
+ both that the copyright notice and this permission notice and warranty
+ disclaimer appear in supporting documentation.
+
+ The author and AMPL Optimization LLC disclaim all warranties with
+ regard to this software, including all implied warranties of
+ merchantability and fitness.  In no event shall the author be liable
+ for any special, indirect or consequential damages or any damages
+ whatsoever resulting from loss of use, data or profits, whether in an
+ action of contract, negligence or other tortious action, arising out
+ of or in connection with the use or performance of this software.
+
+ Author: Victor Zverovich
+ */
+
+#include "expr.cc"
+#include "util.cc"
