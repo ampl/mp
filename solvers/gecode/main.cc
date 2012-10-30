b6b4f14d1525a55f748dee2b66ee6c7d8150003c
viz <viz@dione>
2012-10-30 15:50:26 -0700

    Implement objective support in the gecode driver.

--- solvers/gecode/main.cc
+++ solvers/gecode/main.cc
@@ -1,8 +1,12 @@
-#include "gecode.h"
+#include <iostream>
+#include "solvers/gecode/gecode.h"
 
 int main(int, char **argv) {
-  // TODO: catch exceptions
-  ampl::GecodeDriver d;
-  d.run(argv);
-  return 0;
+  try {
+    ampl::GecodeDriver d;
+    return d.run(argv);
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+  return 1;
 }
