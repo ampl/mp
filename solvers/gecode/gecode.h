629a6f0551e34f5a0ef5e21a2ac9143e7e093411
Victor Zverovich <victor.zverovich@gmail.com>
2013-01-16 09:40:34 -0800

    Clean the gecode interface.

--- solvers/gecode/gecode.h
+++ solvers/gecode/gecode.h
@@ -1,3 +1,25 @@
+/*
+ AMPL solver interface to Gecode.
+
+ Copyright (C) 2012 AMPL Optimization LLC
+
+ Permission to use, copy, modify, and distribute this software and its
+ documentation for any purpose and without fee is hereby granted,
+ provided that the above copyright notice appear in all copies and that
+ both that the copyright notice and this permission notice and warranty
+ disclaimer appear in supporting documentation.
+
+ The author and AMPL Optimization LLC disclaim all warranties with
+ regard to this software, including all implied warranties of
+ merchantability and fitness.  In no event shall the author be liable
+ for any special, indirect or consequential damages or any damages
+ whatsoever resulting from loss of use, data or profits, whether in an
+ action of contract, negligence or other tortious action, arising out
+ of or in connection with the use or performance of this software.
+
+ Author: Victor Zverovich
+ */
+
 #ifndef AMPL_SOLVERS_GECODE_H
 #define AMPL_SOLVERS_GECODE_H
 
@@ -5,9 +27,6 @@
 #include <gecode/minimodel.hh>
 
 #include "solvers/util/solver.h"
-
-struct Option_Info;
-struct ASL_fg;
 
 namespace ampl {
 
@@ -42,21 +61,6 @@ class NLToGecodeConverter :
 
    typedef Gecode::LinExpr LinExpr;
    typedef Gecode::BoolExpr BoolExpr;
-
-   // Creates an integer variable to represent an expression.
-   Gecode::IntVar CreateVar(LinExpr e) {
-     Gecode::IntVar var(problem_,
-         Gecode::Int::Limits::min, Gecode::Int::Limits::max);
-     rel(problem_, var == e);
-     return var;
-   }
-
-   // Creates a boolean variable to represent an expression.
-   Gecode::BoolVar CreateVar(BoolExpr e) {
-     Gecode::BoolVar var(problem_, 0, 1);
-     rel(problem_, var == e);
-     return var;
-   }
 
    BoolExpr Convert(Gecode::BoolOpType op, IteratedLogicalExpr e);
 
