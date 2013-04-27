eca08a1eabfcaf96c7b89b37fe871239ce5bb724
Victor Zverovich <victor.zverovich@gmail.com>
2013-04-26 17:05:32 -0700

    Use StringRef instead of C strings.

--- tests/util.h
+++ tests/util.h
@@ -3,7 +3,18 @@
 
 #include <string>
 
+#include "solvers/util/format.h"
+
 std::string ReadFile(const char *name);
 void WriteFile(const char *name, const char *data);
+
+inline std::string FixPath(fmt::StringRef s) {
+#ifdef WIN32
+  std::string fixed = s;
+  std::replace(fixed.begin(), fixed.end(), '/', '\\');
+  return fixed;
+#endif
+  return s;
+}
 
 #endif  // TESTS_UTIL_H_
