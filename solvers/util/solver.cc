8353d6336baabe843c891a3cc241ec7bbaf93422
Victor Zverovich <victor.zverovich@gmail.com>
2013-06-21 17:01:55 -0700

    Simplify option getters.

--- solvers/util/solver.cc
+++ solvers/util/solver.cc
@@ -99,6 +99,10 @@ std::string Format(fmt::StringRef s, int indent) {
   return os.str();
 }
 
+const char OptionHelper<int>::TYPE_NAME[] = "int";
+const char OptionHelper<double>::TYPE_NAME[] = "double";
+const char OptionHelper<std::string>::TYPE_NAME[] = "string";
+
 int OptionHelper<int>::Parse(const char *&s) {
   char *end = 0;
   long value = std::strtol(s, &end, 10);
