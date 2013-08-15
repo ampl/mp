a3877b7c24ecbae37dec8b4a15283af66592d959
Victor Zverovich <victor.zverovich@gmail.com>
2013-08-15 10:12:03 -0700

    Test appending zero rows to a non-empty table.

--- tests/excel_test.cc
+++ tests/excel_test.cc
@@ -81,6 +81,15 @@ TEST_F(ExcelTest, WriteMaxColumnsExcel2003) {
   handler_->Write(t);
 }
 
+TEST_F(ExcelTest, AppendZeroRows) {
+  Table t("AppendZeroRowsTable", 1, 0, strings_);
+  t = "S",
+       1;
+  handler_->Write(t);
+  t = "S";
+  handler_->Write(t, Handler::INOUT);
+}
+
 #ifndef _WIN64
 // The test fails on 64-bit Windows XP with 64-bit ODBC drivers.
 // According to http://www.microsoft.com/en-us/download/details.aspx?id=13255
