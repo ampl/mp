8a287757caa3fe81e265a8bc0ad1f5453a4cd2e1
Victor Zverovich <victor.zverovich@gmail.com>
2013-08-28 07:47:53 -0700

    Update the company name.

--- solvers/ssdsolver/ssd.cc
+++ solvers/ssdsolver/ssd.cc
@@ -1,7 +1,7 @@
 /*
  Second-order stochastic dominance functions for AMPL.
 
- Copyright (C) 2013 AMPL Optimization LLC
+ Copyright (C) 2013 AMPL Optimization Inc
 
  Permission to use, copy, modify, and distribute this software and its
  documentation for any purpose and without fee is hereby granted,
@@ -9,7 +9,7 @@
  both that the copyright notice and this permission notice and warranty
  disclaimer appear in supporting documentation.
 
- The author and AMPL Optimization LLC disclaim all warranties with
+ The author and AMPL Optimization Inc disclaim all warranties with
  regard to this software, including all implied warranties of
  merchantability and fitness.  In no event shall the author be liable
  for any special, indirect or consequential damages or any damages
