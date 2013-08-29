9484f87568a8cf4a45c018f1690cc850abdfb8f0
Victor Zverovich <victor.zverovich@gmail.com>
2013-08-29 16:49:34 -0700

    Handle random RHS.

--- tests/solvers/smpswriter_test.cc
+++ tests/solvers/smpswriter_test.cc
@@ -32,7 +32,7 @@ TEST(SMPSWriterTest, SMPSOutput) {
   static const char *const EXTS[] = {".cor", ".sto", ".tim"};
   static const char *const PROBLEMS[] = {
       "single-stage", "random-con-matrix", "random-con-matrix2",
-      "zero-core-coefs", "zero-core-con"
+      "random-rhs", "zero-core-coefs", "zero-core-con"
   };
   int count = 0;
   for (size_t i = 0, n = sizeof(PROBLEMS) / sizeof(*PROBLEMS); i != n; ++i) {
@@ -50,7 +50,7 @@ TEST(SMPSWriterTest, SMPSOutput) {
       ;
     }
   }
-  EXPECT_EQ(5 * 3, count);
+  EXPECT_EQ(6 * 3, count);
 }
 
 TEST(SMPSWriterTest, NonlinearNotSupported) {
