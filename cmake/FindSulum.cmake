c3e3a6496cad383395fad63d29f636e2428106c0
Victor Zverovich <victor.zverovich@gmail.com>
2013-10-30 17:36:31 -0700

    Look for optsulum.ampl

--- cmake/FindSulum.cmake
+++ cmake/FindSulum.cmake
@@ -29,14 +29,16 @@ endif ()
 set(SULUM_BIN_DIR ${SULUM_DIR}/${SULUM_SYS}${SULUM_BITS}/bin)
 
 find_path(SULUM_INCLUDE_DIR sulumcpp.h PATHS ${SULUM_DIR}/header)
+find_path(SULUM_AMPL_INCLUDE_DIR optsulum.ampl PATHS ${SULUM_BIN_DIR})
 find_library(SULUM_LIBRARY sulum20 PATHS ${SULUM_BIN_DIR})
 
-set(SULUM_INCLUDE_DIRS ${SULUM_INCLUDE_DIR} ${SULUM_BIN_DIR})
+set(SULUM_INCLUDE_DIRS ${SULUM_INCLUDE_DIR} ${SULUM_AMPL_INCLUDE_DIR})
 set(SULUM_LIBRARIES ${SULUM_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
 # Handle the QUIETLY and REQUIRED arguments and set ODBC_FOUND to TRUE
 # if all listed variables are TRUE.
-find_package_handle_standard_args(SULUM DEFAULT_MSG SULUM_LIBRARY SULUM_INCLUDE_DIR)
+find_package_handle_standard_args(SULUM DEFAULT_MSG
+  SULUM_LIBRARY SULUM_INCLUDE_DIR SULUM_AMPL_INCLUDE_DIR)
 
 mark_as_advanced(SULUM_LIBRARY SULUM_INCLUDE_DIR)
