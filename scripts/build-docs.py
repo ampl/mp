37d79a87cf09d1b6f89f40cc5165e4cda203834e
Victor Zverovich <victor.zverovich@gmail.com>
2014-01-27 12:08:33 -0800

    Complete build-docs.py and remove the old shell script.

--- scripts/build-docs.py
+++ scripts/build-docs.py
@@ -2,31 +2,49 @@
 # This script builds the HTML documentation and commits it to the
 # ampl.github.io repository.
 
-import os
+import errno, os, shutil
 from subprocess import check_call
-from shutil import rmtree
 from glob import glob
 
 REPO = 'ampl.github.io'
 
-rmtree('html')
-rmtree(REPO)
+# Delete an entire directory tree if it exists.
+def rmtree_if_exists(path):
+  try:
+    shutil.rmtree(path)
+  except OSError as e:
+    if e.errno != errno.ENOENT:
+      raise
+
+rmtree_if_exists('html')
+rmtree_if_exists(REPO)
 
 check_call(['cmake', '.'])
 check_call(['make', 'doc'])
 
+# Clone the repo and remove generated content.
 check_call(['git', 'clone', 'git@github.com:ampl/' + REPO + '.git'])
 for entry in glob(REPO + '/*'):
-  if entry == REPO + '/demo' or entry == REPO + '/ampl-book.pdf':
+  basename = os.path.basename(entry)
+  if basename == 'demo' or basename == 'ampl-book.pdf':
     continue
-  if os.path.isfile(entry):
-    os.remove(entry)
-  else:
+  if os.path.isdir(entry):
     shutil.rmtree(entry)
+  else:
+    os.remove(entry)
+
+# Recursively copy an entire directory tree rooted at src.
+# A directory with the same basename as src is created in dst
+# which must name an existing directory.
+def copytodir(src, dst):
+  shutil.copytree(src, os.path.join(dst, os.path.basename(src)))
 
 for entry in glob('html/*'):
-  os.copytree(entry, REPO)
-os.copytree('models', REPO)
+  if os.path.isdir(entry):
+    copytodir(entry, REPO)
+  else:
+    shutil.copy(entry, REPO)
+copytodir('models', REPO)
 
 check_call(['git', 'add', '--all'], cwd=REPO)
 check_call(['git', 'commit', '-m', 'Update documentation'], cwd=REPO)
