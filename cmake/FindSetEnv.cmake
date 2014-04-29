930077d4e44ac79d243da6522ad7945aae2178e0
Victor Zverovich <victor.zverovich@gmail.com>
2014-04-29 16:45:48 -0700

    Split long lines.

--- cmake/FindSetEnv.cmake
+++ cmake/FindSetEnv.cmake
@@ -1,7 +1,9 @@
 # A CMake script to find SetEnv.cmd.
 
+set(winsdk_key
+  "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows")
 find_program(WINSDK_SETENV NAMES SetEnv.cmd
-  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows;CurrentInstallFolder]/bin")
+  PATHS "[${winsdk_key};CurrentInstallFolder]/bin")
 if (WINSDK_SETENV AND PRINT_PATH)
   execute_process(COMMAND ${CMAKE_COMMAND} -E echo "${WINSDK_SETENV}")
 endif ()
