456444632b3b85ee150985bcb125408d8d0aa8fb
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-02 17:05:53 -0700

    Don't use Google Code. Add Ipopt.

--- scripts/make-release.py
+++ scripts/make-release.py
@@ -11,7 +11,6 @@ named "temp".
 """
 
 import datetime, os, re, shutil, sys, tempfile, zipfile
-import googlecode_upload
 from docopt import docopt
 from subprocess import call, check_call
 
@@ -21,8 +20,9 @@ project = "ampl"
 summaries = {
   "amplgsl": "AMPL bindings for the GNU Scientific Library",
   "ampltabl": "ODBC table handler",
-  "cbc": "COIN-OR CBC solver",
+  "cbc": "COIN-OR Cbc solver",
   "gecode": "Gecode solver",
+  "ipopt": "COIN-OR Ipopt solver",
   "jacop": "JaCoP solver"
 }
 
@@ -40,8 +40,10 @@ def rmtree_if_exists(path):
     shutil.rmtree(path)
 
 def upload(filename, summary, labels, simulate):
-  "Upload a file to Google Code."
+  "Upload a file to the server."
   print("Uploading {}".format(filename))
+  return
+  # TODO: implement
   from netrc import netrc
   authenticators = netrc().authenticators("code.google.com")
   username = authenticators[0]
@@ -54,7 +56,7 @@ def upload(filename, summary, labels, simulate):
     print('Google Code upload error: {} ({})'.format(reason, status))
 
 def archive_and_upload(tempdir, simulate):
-  "Create archives and upload them to Google Code."
+  "Create archives and upload them to the server."
   dir = os.path.join(tempdir, 'files')
   gecode_version = None
   for platform in reversed(sorted(sys2label.keys())):
@@ -84,12 +86,14 @@ def archive_and_upload(tempdir, simulate):
     extra_files = {
       'amplgsl': ['gsl.ampl'],
       'gecode': ['gecode.ampl'],
+      'ipopt': ['libipoptfort.dll:win', 'libblas.dll:win', 'libgfortran-3.dll:win',
+                'libquadmath-0.dll:win', 'libgcc_s_seh-1.dll:win64', 'libgcc_s_dw2-1.dll:win32'],
       'jacop': ['ampljacop.jar', 'JaCoP-' + jacop_version + '.jar']
     }
     file_package = {'cp.ampl': None, 'cp.dll': None}
     for package, files in extra_files.iteritems():
       for f in files:
-        file_package[f] = package
+        file_package[f.split(':')[0]] = package
     # Upload individual files.
     paths = []
     for base, dirs, files in os.walk(dir):
@@ -106,8 +110,12 @@ def archive_and_upload(tempdir, simulate):
           files = extra_files.get(basename)
           if files:
             for f in files:
-              extra_path = os.path.join(base, f)
-              zip.write(extra_path, f)
+              items = f.split(':')
+              filename = items[0]
+              if len(items) > 1 and not platform.startswith(items[1]):
+                continue
+              extra_path = os.path.join(base, filename)
+              zip.write(extra_path, filename)
               paths.append(extra_path)
         upload(archive_name, summaries[basename], [sys2label[platform]], simulate)
         paths.append(path)
@@ -145,15 +153,15 @@ if __name__ == '__main__':
   try:
     versions = archive_and_upload(tempdir, simulate)
     # Update the pages that redirect to the downloads for the most recent versions of binaries.
-    check_call(["git", "clone", "https://code.google.com/p/ampl.wiki/"], cwd=tempdir)
-    ampl_wiki_path = os.path.join(tempdir, "ampl.wiki")
-    update_redir_page(ampl_wiki_path, 'ampltabl', versions)
-    update_redir_page(ampl_wiki_path, 'gecode', versions)
-    update_redir_page(ampl_wiki_path, 'jacop', versions)
-    if call(["git", "diff-index", "--quiet", "HEAD"], cwd=ampl_wiki_path):
-      check_call(["git", "commit", "-a", "-m", "update versions"], cwd=ampl_wiki_path)
-      if not simulate:
-        check_call(["git", "push"], cwd=ampl_wiki_path)
+    #check_call(["git", "clone", "https://code.google.com/p/ampl.wiki/"], cwd=tempdir)
+    #ampl_wiki_path = os.path.join(tempdir, "ampl.wiki")
+    #update_redir_page(ampl_wiki_path, 'ampltabl', versions)
+    #update_redir_page(ampl_wiki_path, 'gecode', versions)
+    #update_redir_page(ampl_wiki_path, 'jacop', versions)
+    #if call(["git", "diff-index", "--quiet", "HEAD"], cwd=ampl_wiki_path):
+    #  check_call(["git", "commit", "-a", "-m", "update versions"], cwd=ampl_wiki_path)
+    #  if not simulate:
+    #    check_call(["git", "push"], cwd=ampl_wiki_path)
   finally:
     if not simulate:
       shutil.rmtree(tempdir)
