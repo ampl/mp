01c5aed1dde13ace8c5e869736d9b209d3f91020
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-06 20:22:30 -0400

    Add vagrant config

---
+++ scripts/bootstrap-ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Set up build environment on Ubuntu.
+
+set -e
+
+sudo apt-get update
+
+# Install build tools.
+sudo apt-get install -y git-core gcc g++ gfortran \
+     ccache make buildbot default-jdk unixodbc-dev
+
+# Install CMake.
+CMAKE=cmake-2.8.12.2-Linux-i386
+wget http://www.cmake.org/files/v2.8/$CMAKE.tar.gz
+tar xzf $CMAKE.tar.gz
+sudo rm -rf /opt/$CMAKE
+sudo mv $CMAKE /opt
+sudo ln -sf /opt/$CMAKE/bin/cmake /usr/local/bin
+
+# Set up ccache links.
+sudo ln -sf /usr/bin/ccache /usr/local/bin/gcc
+sudo ln -sf /usr/bin/ccache /usr/local/bin/cc
+sudo ln -sf /usr/bin/ccache /usr/local/bin/g++
+sudo ln -sf /usr/bin/ccache /usr/local/bin/c++
+
+# Install f90cache.
+rm -rf f90cache
+git clone https://github.com/vitaut/f90cache.git
+cd f90cache
+./configure
+make
+sudo make install
+cd ..
+rm -rf f90cache
+cd /usr/local/bin
+sudo ln -sf f90cache gfortran-4.4
