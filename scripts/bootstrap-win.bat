3d3d5712bb153cdebb0ea82f1ff3ac72da692b50
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-15 10:44:49 -0700

    Don't install SDK if it is installed already.

--- scripts/bootstrap-win.bat
+++ scripts/bootstrap-win.bat
@@ -2,6 +2,6 @@
 cd \vagrant
 
 rem Install Windows SDK.
-if exist opt\win64\winsdk (
+if exist opt\win64\winsdk if not exist "\Program Files\Microsoft SDKs" (
   opt\win64\winsdk\setup.exe -q -params:ADDLOCAL=ALL
 )
