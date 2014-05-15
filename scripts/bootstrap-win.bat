937cdb763c22335ed9b6f85e1bf6e4f9bd20c253
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-15 10:36:57 -0700

    Fix paths.

--- scripts/bootstrap-win.bat
+++ scripts/bootstrap-win.bat
@@ -1,7 +1,7 @@
 
-cd /vagrant
+cd \vagrant
 
 rem Install Windows SDK.
-if exist opt/win64/winsdk (
-  opt/win64/winsdk/setup.exe -q -params:ADDLOCAL=ALL
+if exist opt\win64\winsdk (
+  opt\win64\winsdk\setup.exe -q -params:ADDLOCAL=ALL
 )
