7204e5ab40ec7000d40396a2e6d507bc8c50b673
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-15 10:59:58 -0700

    Add .NET installation to provisioning.

--- scripts/bootstrap-win.bat
+++ scripts/bootstrap-win.bat
@@ -1,7 +1,19 @@
 
 cd \vagrant
 
+rem Install .NET Framework 4 for msbuild.
+if exist opt\win64\dotNetFx40_Full_x86_x64.exe (
+  opt\win64\dotNetFx40_Full_x86_x64.exe /s
+)
+
 rem Install Windows SDK.
 if exist opt\win64\winsdk if not exist "\Program Files\Microsoft SDKs" (
   opt\win64\winsdk\setup.exe -q -params:ADDLOCAL=ALL
 )
+
+rem TODO: install Java, CMake, mingw
+
+rem Install CMake.
+bitsadmin /transfer cmake %
+  http://www.cmake.org/files/v2.8/cmake-2.8.12.2-win32-x86.zip %
+  \vagrant\cmake.zip
