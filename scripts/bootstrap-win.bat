1790610ccbfa3b0304cd670b885b9637a66c67a3
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-15 11:15:14 -0700

    Install JDK.

--- scripts/bootstrap-win.bat
+++ scripts/bootstrap-win.bat
@@ -11,9 +11,19 @@ if exist opt\win64\winsdk if not exist "\Program Files\Microsoft SDKs" (
   opt\win64\winsdk\setup.exe -q -params:ADDLOCAL=ALL
 )
 
-rem TODO: install Java, CMake, mingw
+rem Install 64-bit JDK.
+if exist opt\win64\jdk-7u55-windows-x64.exe (
+  opt\win64\winsdk\jdk-7u55-windows-x64.exe /s
+)
+
+rem Install 32-bit JDK.
+if exist opt\win64\jdk-7u55-windows-i586.exe (
+  opt\win64\winsdk\jdk-7u55-windows-i586.exe /s
+)
 
 rem Install CMake.
 bitsadmin /transfer cmake %
   http://www.cmake.org/files/v2.8/cmake-2.8.12.2-win32-x86.zip %
   \vagrant\cmake.zip
+
+rem TODO: install CMake, mingw
