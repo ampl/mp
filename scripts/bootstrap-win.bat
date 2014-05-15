8dfd1eeaee172e47ebed733b2c9e4391f1730668
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-15 15:08:20 -0700

    Provisioning.

--- scripts/bootstrap-win.bat
+++ scripts/bootstrap-win.bat
@@ -4,7 +4,7 @@ cd \vagrant
 rem Install .NET Framework 4 for msbuild.
 rem This requires vagrant-windows plugin version 1.7.0.pre.2 or later.
 rem See https://github.com/WinRb/vagrant-windows/pull/189
-if not exist "\Windows\Microsoft.NET\Framework64" (
+if not exist "\Windows\Microsoft.NET\Framework64\v4.0.30319" (
   if exist opt\win64\dotNetFx40_Full_x86_x64.exe (
     opt\win64\dotNetFx40_Full_x86_x64.exe /q /norestart
   )
@@ -16,18 +16,25 @@ if not exist "\Program Files\Microsoft SDKs" if exist opt\win64\winsdk (
 )
 
 rem Install 64-bit JDK.
-if exist opt\win64\jdk-7u55-windows-x64.exe (
-  opt\win64\jdk-7u55-windows-x64.exe /s
+if not exist "\Program Files\Java\jdk1.7.0_55" (
+  if exist opt\win64\jdk-7u55-windows-x64.exe (
+    opt\win64\jdk-7u55-windows-x64.exe /s
+  )
 )
 
 rem Install 32-bit JDK.
-if exist opt\win64\jdk-7u55-windows-i586.exe (
-  opt\win64\jdk-7u55-windows-i586.exe /s
+if not exist "\Program Files (x86)\Java\jdk1.7.0_55" (
+  if exist opt\win32\jdk-7u55-windows-i586.exe (
+    opt\win32\jdk-7u55-windows-i586.exe /s
+  )
 )
 
 rem Install CMake.
-bitsadmin /transfer cmake %
-  http://www.cmake.org/files/v2.8/cmake-2.8.12.2-win32-x86.zip %
-  \vagrant\cmake.zip
+if not exist "\Program Files\CMake" (
+  bitsadmin /transfer cmake %
+    http://www.cmake.org/files/v2.8/cmake-2.8.12.2-win32-x86.zip %
+    \vagrant\cmake.zip
+  rem TODO: unpack
+)
 
-rem TODO: install CMake, mingw
+rem TODO: install mingw
