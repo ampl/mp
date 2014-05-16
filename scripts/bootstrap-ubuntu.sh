90232f526b9ac9c97cc477dda195ace03c6a2471
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-16 10:40:02 -0700

    Create master in the home directory.

--- scripts/bootstrap-ubuntu.sh
+++ scripts/bootstrap-ubuntu.sh
@@ -50,7 +50,7 @@ sudo ln -sf f90cache gfortran-4.4
 # Install buildbot.
 if [ `uname -m` = "x86_64" ]; then
   sudo pip -q install buildbot
-  buildbot create-master master
+  buildbot create-master ~/master
 fi
 
 sudo pip -q install buildbot-slave
