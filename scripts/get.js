51e3057c36fdedb2090d62db5358ce7cb80ddfef
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-19 15:05:05 -0700

    Add a download script for windows.

---
+++ scripts/get.js
@@ -0,0 +1,10 @@
+// Poor man's wget for Windows.
+
+var WinHttpReq = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
+WinHttpReq.Open("GET", WScript.Arguments(0), /*async=*/false);
+WinHttpReq.Send();
+BinStream = new ActiveXObject("ADODB.Stream");
+BinStream.Type = 1;
+BinStream.Open();
+BinStream.Write(WinHttpReq.ResponseBody);
+BinStream.SaveToFile(WScript.Arguments(1));
