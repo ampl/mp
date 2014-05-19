3a273384ca9b3f5831b722e457224d14fc537348
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-19 14:39:41 -0700

    Create buildbot user if it doesn't exist.

--- scripts/install-buildbot.sh
+++ scripts/install-buildbot.sh
@@ -2,8 +2,13 @@
 # Install buildbot on Ubuntu.
 
 sudo pip install buildbot
-sudo useradd buildbot --home /var/lib/buildbot --create-home
+
 BUILDBOT_DIR=/var/lib/buildbot
+if id -u buildbot >/dev/null 2>&1; then
+  echo buildbot user exists
+else
+  sudo useradd buildbot
+fi
 sudo -u buildbot buildbot create-master -r $BUILDBOT_DIR
 (crontab -u buildbot -l ||
  echo "@reboot PATH=$PATH:/usr/local/bin buildbot start $BUILDBOT_DIR") |
