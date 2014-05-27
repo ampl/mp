416913fa6e0e68e59241a4db265fdc13f666b50a
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-27 10:16:42 -0700

    Add scripts directory to sys.path.

--- scripts/bootstrap-osx.py
+++ scripts/bootstrap-osx.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # Set up build environment on OS X.
 
+import os, tempfile
+
+vagrant_dir = '/vagrant'
+if os.path.exists(vagrant_dir):
+  import sys
+  sys.path.append(vagrant_dir + '/scripts')
+
 from download import download
-import tempfile
 
 # Install command-line tools for Xcode.
 with download(
@@ -10,7 +16,7 @@ with download(
   'command_line_tools_for_xcode_os_x_mountain_lion_april_2013.dmg') as f:
   dir = tempfile.mkdtemp()
   check_call(['hdiutil', 'attach', f, '-mountpoint', dir])
-  check_call(['sudo', 'installer', '-pkg',
+  check_call(['installer', '-pkg',
               'Command Line Tools (Mountain Lion).mpkg', '-target', '/'])
   check_call(['hdiutil', 'detach', dir])
   os.rmdir(dir)
