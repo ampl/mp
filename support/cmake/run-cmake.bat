582ee90b582607e88c1129d705638ee08779e37a
Victor Zverovich <victor.zverovich@gmail.com>
2014-06-04 12:26:37 -0700

    Fix path.

--- support/cmake/run-cmake.bat
+++ support/cmake/run-cmake.bat
@@ -3,7 +3,7 @@ rem Use this script if you want to build ASL with the Windows SDK toolchain.
 rem It configures build environment and runs CMake forwarding all command-line
 rem arguments to it, for example:
 rem
-rem   cmake\run-cmake -G "Visual Studio 10 Win64" .
+rem   support\cmake\run-cmake -G "Visual Studio 10 Win64" .
 
 set args=%*
 set args=%args:"=\"%
