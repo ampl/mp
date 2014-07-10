3218716f1164f8c9828b1536382e972b38582dbb
Victor Zverovich <victor.zverovich@gmail.com>
2014-07-10 12:11:33 -0700

    Clean ilogcp driver.

---
+++ solvers/ilogcp/main.cc
@@ -0,0 +1,37 @@
+/*
+ AMPL solver interface to IBM/ILOG CP solver.
+
+ Copyright (C) 2012 - 2014 AMPL Optimization Inc
+
+ Permission to use, copy, modify, and distribute this software and its
+ documentation for any purpose and without fee is hereby granted,
+ provided that the above copyright notice appear in all copies and that
+ both that the copyright notice and this permission notice and warranty
+ disclaimer appear in supporting documentation.
+
+ The author and AMPL Optimization Inc disclaim all warranties with
+ regard to this software, including all implied warranties of
+ merchantability and fitness.  In no event shall the author be liable
+ for any special, indirect or consequential damages or any damages
+ whatsoever resulting from loss of use, data or profits, whether in an
+ action of contract, negligence or other tortious action, arising out
+ of or in connection with the use or performance of this software.
+
+ Author: Victor Zverovich
+ */
+
+#include "solvers/ilogcp/ilogcp.h"
+
+int main(int, char **argv) {
+  // Solver should be destroyed after any IloException is handled.
+  std::auto_ptr<ampl::IlogCPSolver> s;
+  try {
+    s.reset(new ampl::IlogCPSolver());
+    return s->Run(argv);
+  } catch (const IloException &e) {
+    fmt::print(stderr, "Error: {}\n", e);
+  } catch (const ampl::Error &e) {
+    fmt::print(stderr, "Error: {}\n", e.what());
+  }
+  return 1;
+}
