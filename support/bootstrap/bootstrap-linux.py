b52033bda88e81ec6e146cfa4815a38731aa1f82
Victor Zverovich <victor.zverovich@gmail.com>
2014-07-16 16:06:25 -0700

    Check if LocalSolver is installed.

--- support/bootstrap/bootstrap-linux.py
+++ support/bootstrap/bootstrap-linux.py
@@ -43,9 +43,10 @@ if __name__ == '__main__':
   version = re.match(r'.* (\d+\.\d+)\.\d+', output).group(1)
   add_to_path('/usr/local/bin/f90cache', 'gfortran-' + version)
 
-  with download('http://www.localsolver.com/downloads/' +
-      'LocalSolver_4_0_20140127_Linux{}.run'.format(64 if x86_64 else 32)) as f:
-    check_call(['sh', f])
+  if not installed('localsolver'):
+    with download('http://www.localsolver.com/downloads/LocalSolver' +
+        '_4_0_20140127_Linux{}.run'.format(64 if x86_64 else 32)) as f:
+      check_call(['sh', f])
 
   copy_optional_dependencies('linux-' + platform.machine())
   
