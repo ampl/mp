f4bc7458fa2d508e55e8372494e86ba0979b6d35
Victor Zverovich <victor.zverovich@gmail.com>
2014-07-16 13:52:48 -0700

    Follow CMake conventions in modules.

--- support/cmake/FindLocalSolver.cmake
+++ support/cmake/FindLocalSolver.cmake
@@ -2,32 +2,32 @@
 #
 # Once done this will define
 #
-#  LOCALSOLVER_FOUND - System has LocalSolver
-#  LOCALSOLVER_INCLUDE_DIRS - The LocalSolver include directories
-#  LOCALSOLVER_LIBRARIES - The libraries needed to use LocalSolver
+#  LocalSolver_FOUND - System has LocalSolver
+#  LocalSolver_INCLUDE_DIRS - The LocalSolver include directories
+#  LocalSolver_LIBRARIES - The libraries needed to use LocalSolver
 
 if (UNIX)
-  set(LOCALSOLVER_DIR /opt)
+  set(LocalSolver_DIR /opt)
 else ()
-  set(LOCALSOLVER_DIR "C:")
+  set(LocalSolver_DIR "C:")
 endif ()
 
-file(GLOB LOCALSOLVER_DIRS "${LOCALSOLVER_DIR}/localsolver*")
-if (LOCALSOLVER_DIRS)
-  list(GET LOCALSOLVER_DIRS 0 LOCALSOLVER_DIR)
-  message(STATUS "Found LocalSolver directory: ${LOCALSOLVER_DIR}")
+file(GLOB LocalSolver_DIRS "${LocalSolver_DIR}/localsolver*")
+if (LocalSolver_DIRS)
+  list(GET LocalSolver_DIRS 0 LocalSolver_DIR)
+  message(STATUS "Found LocalSolver directory: ${LocalSolver_DIR}")
 endif ()
 
-find_path(LOCALSOLVER_INCLUDE_DIR localsolver.h PATHS ${LOCALSOLVER_DIR}/include)
-find_library(LOCALSOLVER_LIBRARY localsolver PATHS ${LOCALSOLVER_DIR}/bin)
+find_path(LocalSolver_INCLUDE_DIR localsolver.h PATHS ${LocalSolver_DIR}/include)
+find_library(LocalSolver_LIBRARY localsolver PATHS ${LocalSolver_DIR}/bin)
 
-set(LOCALSOLVER_INCLUDE_DIRS ${LOCALSOLVER_INCLUDE_DIR})
-set(LOCALSOLVER_LIBRARIES ${LOCALSOLVER_LIBRARY})
+set(LocalSolver_INCLUDE_DIRS ${LocalSolver_INCLUDE_DIR})
+set(LocalSolver_LIBRARIES ${LocalSolver_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
 # Handle the QUIETLY and REQUIRED arguments and set ODBC_FOUND to TRUE
 # if all listed variables are TRUE.
-find_package_handle_standard_args(LOCALSOLVER DEFAULT_MSG
-  LOCALSOLVER_LIBRARY LOCALSOLVER_INCLUDE_DIR)
+find_package_handle_standard_args(LocalSolver DEFAULT_MSG
+  LocalSolver_LIBRARY LocalSolver_INCLUDE_DIR)
 
-mark_as_advanced(LOCALSOLVER_LIBRARY LOCALSOLVER_INCLUDE_DIR)
+mark_as_advanced(LocalSolver_LIBRARY LocalSolver_INCLUDE_DIR)
