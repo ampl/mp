6605d3ae8e997ab6145566d91d9ff270d0986cf7
Victor Zverovich <victor.zverovich@gmail.com>
2014-08-05 11:44:21 -0700

    Fix windows build.

--- solvers/util/os.cc
+++ solvers/util/os.cc
@@ -185,8 +185,9 @@ path ampl::GetExecutablePath() {
 
 ampl::MemoryMappedFile::MemoryMappedFile(fmt::StringRef filename)
 : start_(), size_() {
-  class Handle : Noncopyable {
+  class Handle {
     HANDLE handle_;
+    FMT_DISALLOW_COPY_AND_ASSIGN(Handle);
    public:
     explicit Handle(HANDLE h) : handle_(h) {}
     ~Handle() { CloseHandle(handle_); }
