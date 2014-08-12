cee445ef5b577973450e6adcb80ed02ad9fbb719
Victor Zverovich <victor.zverovich@gmail.com>
2014-08-12 12:58:04 -0700

    Update buildbot config.

--- support/buildbot/update-config.sh
+++ support/buildbot/update-config.sh
@@ -4,4 +4,5 @@
 
 BUILDBOT_DIR=`ssh ampl.com "echo ~buildbot"`
 scp master.cfg ampl.com:$BUILDBOT_DIR/master.cfg
+scp templates/layout.html ampl.com:$BUILDBOT_DIR/templates/layout.html
 ssh -t ampl.com "cd $BUILDBOT_DIR && sudo -H -u buildbot buildbot reconfig"
