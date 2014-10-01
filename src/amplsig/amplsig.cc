2b604455f760fbdbe075f2e5b866ac8af8d5913e
Victor Zverovich <victor.zverovich@gmail.com>
2014-10-01 09:48:28 -0700

    Add license and copyright info

--- src/amplsig/amplsig.cc
+++ src/amplsig/amplsig.cc
@@ -1,10 +1,30 @@
-// AMPL extension DLL that simulates sending SIGINT (Ctrl-C) from another
-// process on Windows. It is required since GenerateConsoleCtrlEvent doesn't
-// work reliably with CTRL_C_EVENT across processes.
-// The DLL can be loaded from AMPL as follows:
-//   load signal.dll;
-// It connects to a socket at 127.0.0.1:$AMPLSIG_PORT and generates
-// CTRL_C_EVENT on every SIGINT command read from the socket input stream.
+/*
+ AMPL extension DLL that simulates sending SIGINT (Ctrl-C) from another
+ process on Windows. It is required since GenerateConsoleCtrlEvent doesn't
+ work reliably with CTRL_C_EVENT across processes.
+ The DLL can be loaded from AMPL as follows:
+   load signal.dll;
+ It connects to a socket at 127.0.0.1:$AMPLSIG_PORT and generates
+ CTRL_C_EVENT on every SIGINT command read from the socket input stream.
+
+ Copyright (C) 2014 AMPL Optimization Inc
+
+ Permission to use, copy, modify, and distribute this software and its
+ documentation for any purpose and without fee is hereby granted,
+ provided that the above copyright notice appear in all copies and that
+ both that the copyright notice and this permission notice and warranty
+ disclaimer appear in supporting documentation.
+
+ The author and AMPL Optimization Inc disclaim all warranties with
+ regard to this software, including all implied warranties of
+ merchantability and fitness.  In no event shall the author be liable
+ for any special, indirect or consequential damages or any damages
+ whatsoever resulting from loss of use, data or profits, whether in an
+ action of contract, negligence or other tortious action, arising out
+ of or in connection with the use or performance of this software.
+
+ Author: Victor Zverovich
+ */
 
 #include <cstdio>
 #include <cstdlib>
