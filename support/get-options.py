45a07e2c1d0d0a8258ff72adb7602de53a74e05d
Victor Zverovich <victor.zverovich@gmail.com>
2014-10-29 16:05:30 -0700

    Update option extraction script

--- support/get-options.py
+++ support/get-options.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # This scripts extract options from solvers and formats them in HTML.
 
-import os, errno
+import errno, os, sys
 from ctypes import cdll, Structure, c_int, c_char_p, c_void_p, byref
 from docutils.core import publish_parts
 from docutils.parsers.rst import directives
@@ -14,7 +14,7 @@ class SolverOption:
     self.description = description
     self.values = values
 
-class ASL_SolverOptionInfo(Structure):
+class MP_SolverOptionInfo(Structure):
   _fields_ = [("name", c_char_p),
               ("description", c_char_p),
               ("flags", c_int),
@@ -25,7 +25,7 @@ class OptionValueInfo:
     self.value = value
     self.description = description
 
-class ASL_OptionValueInfo(Structure):
+class MP_OptionValueInfo(Structure):
   _fields_ = [("value", c_char_p),
               ("description", c_char_p)]
   
@@ -33,44 +33,44 @@ class Solver:
   def __init__(self, libpath):
     self.lib = cdll.LoadLibrary(libpath)
     error = c_void_p()
-    self.solver = self.lib.ASL_CreateSolver(byref(error))
-    self.lib.ASL_GetErrorMessage.restype = c_char_p
+    self.solver = self.lib.MP_CreateSolver(byref(error))
+    self.lib.MP_GetErrorMessage.restype = c_char_p
     if not self.solver:
-      raise Exception(self.lib.ASL_GetErrorMessage(error))
+      raise Exception(self.lib.MP_GetErrorMessage(error))
 
   def __enter__(self):
     return self
 
   def __exit__(self, type, value, traceback):
-    self.lib.ASL_DestroySolver(self.solver)
+    self.lib.MP_DestroySolver(self.solver)
 
   def _check(self, success):
     if not success:
-      error = self.lib.ASL_GetLastError(self.solver)
-      raise Exception(self.lib.ASL_GetErrorMessage(error))
+      error = self.lib.MP_GetLastError(self.solver)
+      raise Exception(self.lib.MP_GetErrorMessage(error))
     
   def option_header(self):
-    self.lib.ASL_GetOptionHeader.restype = c_char_p
-    header = self.lib.ASL_GetOptionHeader(self.solver)
+    self.lib.MP_GetOptionHeader.restype = c_char_p
+    header = self.lib.MP_GetOptionHeader(self.solver)
     self._check(header is not None)
     return header
 
   def get_options(self):
-    num_options = self.lib.ASL_GetSolverOptions(self.solver, None, 0)
+    num_options = self.lib.MP_GetSolverOptions(self.solver, None, 0)
     self._check(num_options != -1)
-    info = (ASL_SolverOptionInfo * num_options)()
-    num_options = self.lib.ASL_GetSolverOptions(self.solver, info, num_options)
+    info = (MP_SolverOptionInfo * num_options)()
+    num_options = self.lib.MP_GetSolverOptions(self.solver, info, num_options)
     self._check(num_options != -1)
     options = []
     for i in info:
       num_values = 0
       if (i.flags & 1) != 0:
-        num_values = self.lib.ASL_GetOptionValues(self.solver, i.option, None, 0)
+        num_values = self.lib.MP_GetOptionValues(self.solver, i.option, None, 0)
         self._check(num_values != -1)
       values = []
       if num_values != 0:
-        asl_values = (ASL_OptionValueInfo * num_values)()
-        num_values = self.lib.ASL_GetOptionValues(self.solver, i.option, asl_values, num_values)
+        asl_values = (MP_OptionValueInfo * num_values)()
+        num_values = self.lib.MP_GetOptionValues(self.solver, i.option, asl_values, num_values)
         self._check(num_values != -1)
         for v in asl_values:
           values.append(OptionValueInfo(v.value, v.description))
@@ -108,7 +108,7 @@ class ValueTableDirective(rst.Directive):
 
 directives.register_directive('value-table', ValueTableDirective)
 
-solver_names = ['Gecode', 'IlogCP', 'Jacop', 'SSDSolver', 'Sulum']
+solver_names = ['Gecode', 'IlogCP', 'Jacop', 'LocalSolver', 'SSDSolver', 'Sulum']
 for solver_name in solver_names:
   solver_name_lower = solver_name.lower()
   html_filename = solver_name_lower + '-options.html'
@@ -117,7 +117,7 @@ for solver_name in solver_names:
   except OSError as e:
     if e.errno != errno.ENOENT:
       raise
-  libname = '../solvers/{0}/libampl{0}.so'.format(solver_name_lower)
+  libname = '{}/libampl{}.so'.format(sys.argv[1], solver_name_lower)
   if not os.path.exists(libname):
     continue
   with Solver(libname) as solver:
