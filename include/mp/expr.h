d0fcf9c83a00aa9f5b3721ca23e994e524eeaa0d
Victor Zverovich <victor.zverovich@gmail.com>
2014-11-04 11:09:24 -0800

    Implement new expression API

--- include/mp/expr.h
+++ include/mp/expr.h
@@ -37,12 +37,37 @@ struct ExprImpl {
   expr::Kind kind;
 };
 
+struct NumericConstantImpl : ExprImpl {
+  double value;
+};
+
+struct VariableImpl : ExprImpl {
+  int index;
+};
+
 struct UnaryExprImpl : ExprImpl {
   const ExprImpl *arg;
 };
 
-struct NumericConstantImpl : ExprImpl {
-  double value;
+struct BinaryExprImpl : ExprImpl {
+  const ExprImpl *lhs;
+  const ExprImpl *rhs;
+};
+
+struct IfExprImpl : ExprImpl {
+  const ExprImpl *condition;
+  const ExprImpl *true_expr;
+  const ExprImpl *false_expr;
+};
+
+struct PLTermImpl : ExprImpl {
+  int num_breakpoints;
+  int var_index;
+  double data[1];
+};
+
+struct LogicalConstantImpl : ExprImpl {
+  bool value;
 };
 
 // Returns true if the non-null expression e is of type ExprClass.
@@ -86,7 +111,8 @@ class Expr {
   // Creates an expression from an implementation.
   template <typename TargetExpr>
   static TargetExpr Create(const internal::ExprImpl *impl) {
-    assert(!impl || internal::Is<TargetExpr>(impl->kind));
+    assert((!impl || internal::Is<TargetExpr>(impl->kind)) &&
+           "invalid expression kind");
     TargetExpr expr;
     expr.impl_ = impl;
     return expr;
@@ -115,41 +141,135 @@ class Expr {
   operator SafeBool() const { return impl_ != 0 ? &Expr::True : 0; }
 };
 
+#define MP_EXPR(Impl) \
+ private: \
+  const internal::Impl *impl() const { \
+    return static_cast<const internal::Impl*>(this->impl_); \
+  }
+
 MP_SPECIALIZE_IS_RANGE(Expr, EXPR)
 
 // A numeric expression.
 class NumericExpr : public Expr {};
-
 MP_SPECIALIZE_IS_RANGE(NumericExpr, NUMERIC)
 
-// An unary numeric expression.
-class UnaryExpr : public NumericExpr {
- private:
-  const internal::UnaryExprImpl *impl() const {
-    return static_cast<const internal::UnaryExprImpl*>(impl_);
-  }
-
- public:
-  // Returns the argument of this expression.
-  NumericExpr arg() const { return Create<NumericExpr>(impl()->arg); }
-};
-
-MP_SPECIALIZE_IS_RANGE(UnaryExpr, UNARY)
+// A logical expression.
+class LogicalExpr : public Expr {};
+MP_SPECIALIZE_IS_RANGE(LogicalExpr, LOGICAL)
 
 // A numeric constant.
 // Examples: 42, -1.23e-4
 class NumericConstant : public NumericExpr {
- private:
-  const internal::NumericConstantImpl *impl() const {
-    return static_cast<const internal::NumericConstantImpl*>(impl_);
-  }
-
+  MP_EXPR(NumericConstantImpl)
  public:
   // Returns the value of this constant.
   double value() const { return impl()->value; }
 };
 
 MP_SPECIALIZE_IS(NumericConstant, CONSTANT)
+
+// A reference to a variable.
+// Example: x
+class Variable : public NumericExpr {
+  MP_EXPR(VariableImpl)
+ public:
+  // Returns the index of the referenced variable.
+  int index() const { return impl()->index; }
+};
+
+MP_SPECIALIZE_IS(Variable, VARIABLE)
+
+// A unary expression.
+// Base: base expression class.
+template <typename Base>
+class BasicUnaryExpr : public Base {
+  MP_EXPR(UnaryExprImpl)
+ public:
+  // Returns the argument of this expression.
+  Base arg() const { return Expr::Create<Base>(impl()->arg); }
+};
+
+// A unary numeric expression.
+// Examples: -x, sin(x), where x is a variable.
+typedef BasicUnaryExpr<NumericExpr> UnaryExpr;
+MP_SPECIALIZE_IS_RANGE(UnaryExpr, UNARY)
+
+// A binary expression.
+// Base: base expression class.
+// Arg: argument expression class.
+template <typename Base, typename Arg = Base>
+class BasicBinaryExpr : public Base {
+  MP_EXPR(BinaryExprImpl)
+ public:
+  // Returns the left-hand side (the first argument) of this expression.
+  Arg lhs() const { return Expr::Create<Arg>(impl()->lhs); }
+
+  // Returns the right-hand side (the second argument) of this expression.
+  Arg rhs() const { return Expr::Create<Arg>(impl()->rhs); }
+};
+
+// A binary numeric expression.
+// Examples: x / y, atan2(x, y), where x and y are variables.
+typedef BasicBinaryExpr<NumericExpr> BinaryExpr;
+MP_SPECIALIZE_IS_RANGE(BinaryExpr, BINARY)
+
+template <typename Base>
+class BasicIfExpr : public Base {
+  MP_EXPR(IfExprImpl)
+ public:
+  LogicalExpr condition() const {
+    return Expr::Create<LogicalExpr>(impl()->condition);
+  }
+
+  Base true_expr() const { return Expr::Create<Base>(impl()->true_expr); }
+  Base false_expr() const { return Expr::Create<Base>(impl()->false_expr); }
+};
+
+// An if-then-else expression.
+// Example: if x != 0 then y else z, where x, y and z are variables.
+typedef BasicIfExpr<NumericExpr> IfExpr;
+MP_SPECIALIZE_IS(IfExpr, IF)
+
+// A piecewise-linear term.
+// Example: <<0; -1, 1>> x, where x is a variable.
+class PLTerm : public NumericExpr {
+  MP_EXPR(PLTermImpl)
+ public:
+  // Returns the number of breakpoints in this term.
+  int num_breakpoints() const { return impl()->num_breakpoints; }
+
+  // Returns the number of slopes in this term.
+  int num_slopes() const { return num_breakpoints() + 1; }
+
+  // Returns a breakpoint with the specified index.
+  double breakpoint(int index) const {
+    assert(index >= 0 && index < num_breakpoints());
+    return impl()->data[2 * index + 1];
+  }
+
+  // Returns a slope with the specified index.
+  double slope(int index) const {
+    assert(index >= 0 && index < num_slopes());
+    return impl()->data[2 * index];
+  }
+
+  int var_index() const { return impl()->var_index; }
+};
+
+MP_SPECIALIZE_IS(PLTerm, PLTERM)
+
+// TODO: numeric expressions
+
+// A logical constant.
+// Examples: 0, 1
+class LogicalConstant : public LogicalExpr {
+  MP_EXPR(LogicalConstantImpl)
+ public:
+  // Returns the value of this constant.
+  bool value() const { return impl()->value; }
+};
+
+MP_SPECIALIZE_IS(LogicalConstant, CONSTANT)
 
 class ExprFactory {
  private:
@@ -159,12 +279,15 @@ class ExprFactory {
 
   // Allocates memory for an object of type Impl which is a subclass
   // of ExprImpl.
+  // extra_bytes: extra bytes to allocate at the end.
   template <typename Impl>
-  Impl *Allocate() {
+  Impl *Allocate(expr::Kind kind, int extra_bytes = 0) {
     // Call push_back first to make sure that the impl pointer doesn't leak
     // if push_back throws an exception.
     exprs_.push_back(0);
-    Impl *impl = new Impl();
+    Impl *impl = reinterpret_cast<Impl*>(
+          ::operator new(sizeof(Impl) + extra_bytes));
+    impl->kind = kind;
     exprs_.back() = impl;
     return impl;
   }
@@ -175,20 +298,103 @@ class ExprFactory {
     // TODO: delete expressions
   }
 
-  // Makes an unary expression.
+  // Makes a numeric constant.
+  NumericConstant MakeNumericConstant(double value) {
+    internal::NumericConstantImpl *impl =
+        Allocate<internal::NumericConstantImpl>(expr::CONSTANT);
+    impl->value = value;
+    return Expr::Create<NumericConstant>(impl);
+  }
+
+  // Makes a variable reference.
+  Variable MakeVariable(int index) {
+    internal::VariableImpl *impl =
+        Allocate<internal::VariableImpl>(expr::VARIABLE);
+    impl->index = index;
+    return Expr::Create<Variable>(impl);
+  }
+
+  // Makes a unary expression.
   UnaryExpr MakeUnary(expr::Kind kind, NumericExpr arg) {
-    internal::UnaryExprImpl *impl = Allocate<internal::UnaryExprImpl>();
-    impl->kind = kind;
+    internal::UnaryExprImpl *impl = Allocate<internal::UnaryExprImpl>(kind);
     impl->arg = arg.impl_;
     return Expr::Create<UnaryExpr>(impl);
   }
 
-  // Makes a numeric constant.
-  NumericConstant MakeNumericConstant(double value) {
-    internal::NumericConstantImpl *impl =
-        Allocate<internal::NumericConstantImpl>();
+  // Makes a binary expression.
+  BinaryExpr MakeBinary(expr::Kind kind, NumericExpr lhs, NumericExpr rhs) {
+    internal::BinaryExprImpl *impl = Allocate<internal::BinaryExprImpl>(kind);
+    impl->lhs = lhs.impl_;
+    impl->rhs = rhs.impl_;
+    return Expr::Create<BinaryExpr>(impl);
+  }
+
+  // Makes an if expression.
+  IfExpr MakeIf(LogicalExpr condition,
+                NumericExpr true_expr, NumericExpr false_expr) {
+    internal::IfExprImpl *impl = Allocate<internal::IfExprImpl>(expr::IF);
+    impl->condition = condition.impl_;
+    impl->true_expr = true_expr.impl_;
+    impl->false_expr = false_expr.impl_;
+    return Expr::Create<IfExpr>(impl);
+  }
+
+  // A piecewise-linear term builder.
+  class PLTermBuilder {
+   private:
+    internal::PLTermImpl *impl_;
+    int slope_index_;
+    int breakpoint_index_;
+
+    friend class ExprFactory;
+
+   public:
+    explicit PLTermBuilder(internal::PLTermImpl *impl)
+      : impl_(impl), slope_index_(0), breakpoint_index_(0) {}
+
+    void AddSlope(double slope) {
+      assert(slope_index_ < impl_->num_breakpoints + 1 && "too many slopes");
+      impl_->data[2 * slope_index_] = slope;
+      ++slope_index_;
+    }
+
+    void AddBreakpoint(double breakpoint) {
+      assert(breakpoint_index_ < impl_->num_breakpoints &&
+             "too many breakpoints");
+      impl_->data[2 * breakpoint_index_ + 1] = breakpoint;
+      ++breakpoint_index_;
+    }
+  };
+
+  // Begins building a piecewise-linear term.
+  PLTermBuilder BeginPLTerm(int num_breakpoints) {
+    assert(num_breakpoints > 0 && "invalid number of breakpoints");
+    internal::PLTermImpl *impl = Allocate<internal::PLTermImpl>(
+          expr::PLTERM, sizeof(double) * num_breakpoints * 2);
+    impl->num_breakpoints = num_breakpoints;
+    return PLTermBuilder(impl);
+  }
+
+  // Ends building a piecewise-linear term.
+  PLTerm EndPLTerm(PLTermBuilder builder, Variable var) {
+    internal::PLTermImpl *impl = builder.impl_;
+    impl->var_index = var.index();
+    // Make sure that all slopes and breakpoints provided.
+    assert(builder.slope_index_ == impl->num_breakpoints + 1 &&
+           "too few slopes");
+    assert(builder.breakpoint_index_ == impl->num_breakpoints &&
+           "too few breakpoints");
+    return Expr::Create<PLTerm>(impl);
+  }
+
+  // TODO: more numeric expressions
+
+  // Makes a logical constant.
+  LogicalConstant MakeLogicalConstant(bool value) {
+    internal::LogicalConstantImpl *impl =
+        Allocate<internal::LogicalConstantImpl>(expr::CONSTANT);
     impl->value = value;
-    return Expr::Create<NumericConstant>(impl);
+    return Expr::Create<LogicalConstant>(impl);
   }
 };
 }  // namespace mp
--- test/expr-test.cc
+++ test/expr-test.cc
@@ -28,9 +28,133 @@ TEST(ExprTest, Expr) {
   EXPECT_TRUE(e == 0);
 }
 
-TEST(ExprTest, UnaryExpr) {
+TEST(ExprTest, NumericExpr) {
+  mp::NumericExpr e;
+  EXPECT_TRUE(e == 0);
+}
+
+TEST(ExprTest, LogicalExpr) {
+  mp::LogicalExpr e;
+  EXPECT_TRUE(e == 0);
+}
+
+TEST(ExprTest, NumericConstant) {
+  mp::NumericConstant e;
+  EXPECT_TRUE(e == 0);
   mp::ExprFactory factory;
-  mp::UnaryExpr e = factory.MakeUnary(mp::expr::ABS, mp::NumericExpr());
+  e = factory.MakeNumericConstant(1.23);
+  EXPECT_EQ(mp::expr::CONSTANT, e.kind());
+  EXPECT_TRUE(e != 0);
+  EXPECT_EQ(1.23, e.value());
+}
+
+TEST(ExprTest, Variable) {
+  mp::Variable e;
+  EXPECT_TRUE(e == 0);
+  mp::ExprFactory factory;
+  e = factory.MakeVariable(42);
+  EXPECT_EQ(mp::expr::VARIABLE, e.kind());
+  EXPECT_TRUE(e != 0);
+  EXPECT_EQ(42, e.index());
+}
+
+TEST(ExprTest, UnaryExpr) {
+  mp::UnaryExpr e;
+  EXPECT_TRUE(e == 0);
+  mp::ExprFactory factory;
+  auto arg = factory.MakeNumericConstant(42);
+  e = factory.MakeUnary(mp::expr::ABS, arg);
   EXPECT_TRUE(e != 0);
   EXPECT_EQ(mp::expr::ABS, e.kind());
+  EXPECT_EQ(arg, e.arg());
+  EXPECT_DEBUG_DEATH(factory.MakeUnary(mp::expr::ADD, arg),
+                     "invalid expression kind");
 }
+
+TEST(ExprTest, BinaryExpr) {
+  mp::BinaryExpr e;
+  EXPECT_TRUE(e == 0);
+  mp::ExprFactory factory;
+  auto lhs = factory.MakeNumericConstant(42);
+  auto rhs = factory.MakeVariable(0);
+  e = factory.MakeBinary(mp::expr::MUL, lhs, rhs);
+  EXPECT_TRUE(e != 0);
+  EXPECT_EQ(mp::expr::MUL, e.kind());
+  EXPECT_EQ(lhs, e.lhs());
+  EXPECT_EQ(rhs, e.rhs());
+  EXPECT_DEBUG_DEATH(factory.MakeBinary(mp::expr::IF, lhs, rhs),
+                     "invalid expression kind");
+}
+
+TEST(ExprTest, IfExpr) {
+  mp::IfExpr e;
+  EXPECT_TRUE(e == 0);
+  mp::ExprFactory factory;
+  auto condition = factory.MakeLogicalConstant(true);
+  auto true_expr = factory.MakeNumericConstant(42);
+  auto false_expr = factory.MakeVariable(0);
+  e = factory.MakeIf(condition, true_expr, false_expr);
+  EXPECT_TRUE(e != 0);
+  EXPECT_EQ(mp::expr::IF, e.kind());
+  EXPECT_EQ(condition, e.condition());
+  EXPECT_EQ(true_expr, e.true_expr());
+  EXPECT_EQ(false_expr, e.false_expr());
+}
+
+TEST(ExprTest, PLTerm) {
+  mp::PLTerm e;
+  EXPECT_TRUE(e == 0);
+  mp::ExprFactory factory;
+  mp::ExprFactory::PLTermBuilder builder = factory.BeginPLTerm(2);
+  builder.AddSlope(11);
+  builder.AddBreakpoint(111);
+  builder.AddSlope(22);
+  builder.AddBreakpoint(222);
+  builder.AddSlope(33);
+  e = factory.EndPLTerm(builder, factory.MakeVariable(42));
+  EXPECT_EQ(mp::expr::PLTERM, e.kind());
+  EXPECT_EQ(2, e.num_breakpoints());
+  EXPECT_EQ(3, e.num_slopes());
+  EXPECT_EQ(11, e.slope(0));
+  EXPECT_EQ(22, e.slope(1));
+  EXPECT_EQ(33, e.slope(2));
+  EXPECT_EQ(111, e.breakpoint(0));
+  EXPECT_EQ(222, e.breakpoint(1));
+  EXPECT_EQ(42, e.var_index());
+  EXPECT_DEBUG_DEATH(factory.BeginPLTerm(0), "invalid number of breakpoints");
+}
+
+TEST(ExprTest, TooManyBreakpoints) {
+  mp::ExprFactory factory;
+  auto builder = factory.BeginPLTerm(1);
+  builder.AddBreakpoint(0);
+  EXPECT_DEBUG_DEATH(builder.AddBreakpoint(1), "too many breakpoints");
+}
+
+TEST(ExprTest, TooManySlopes) {
+  mp::ExprFactory factory;
+  auto builder = factory.BeginPLTerm(1);
+  builder.AddSlope(0);
+  builder.AddSlope(1);
+  EXPECT_DEBUG_DEATH(builder.AddSlope(2), "too many slopes");
+}
+
+TEST(ExprTest, TooFewBreakpoints) {
+  mp::ExprFactory factory;
+  auto builder = factory.BeginPLTerm(1);
+  builder.AddSlope(0);
+  builder.AddSlope(1);
+  EXPECT_DEBUG_DEATH(factory.EndPLTerm(builder, factory.MakeVariable(0)),
+                     "too few breakpoints");
+}
+
+TEST(ExprTest, TooFewSlopes) {
+  mp::ExprFactory factory;
+  auto builder = factory.BeginPLTerm(1);
+  builder.AddBreakpoint(0);
+  builder.AddSlope(0);
+  EXPECT_DEBUG_DEATH(factory.EndPLTerm(builder, factory.MakeVariable(0)),
+                     "too few slopes");
+}
+
+// TODO
