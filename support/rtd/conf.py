7ccc26ea0a71a1f0e0c411f5c97d258a3b07e232
vitaut <victor.zverovich@gmail.com>
2015-03-23 10:44:09 -0700

    Try using absolute path to templates because of issues with RTD

--- support/rtd/conf.py
+++ support/rtd/conf.py
@@ -1,5 +1,7 @@
 # Sphinx configuration for readthedocs.
 
+import os
+
 master_doc = 'index'
 html_theme = 'sphinxdoc'
-templates_path = ['_templates']
+templates_path = [os.path.join(os.path.dirname(os.path.realpath(__file__)), '_templates')]
