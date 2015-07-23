d3f3d992197941998e7d3b97e3d7ec0bf6782ac4
Dominique Orban <dominique.orban@gmail.com>
2015-07-23 18:24:11 -0400

    Simple interface: catch nonzero error flags.

--- src/asl/aslinterface.h
+++ src/asl/aslinterface.h
@@ -41,23 +41,23 @@ double *asl_uvar(ASL *asl);
 double *asl_lcon(ASL *asl);
 double *asl_ucon(ASL *asl);
 
-void asl_varscale(ASL *asl, double *s);
-void asl_lagscale(ASL *asl, double  s);
-void asl_conscale(ASL *asl, double *s);
+void asl_varscale(ASL *asl, double *s, int *err);
+void asl_lagscale(ASL *asl, double  s, int *err);
+void asl_conscale(ASL *asl, double *s, int *err);
 
-double  asl_obj(     ASL *asl, double *x);
-void    asl_grad(    ASL *asl, double *x, double *g);
-void    asl_cons(    ASL *asl, double *x, double *c);
-double  asl_jcon(    ASL *asl, double *x, int j);
-void    asl_jcongrad(ASL *asl, double *x, double *g, int j);
+double  asl_obj(     ASL *asl, double *x, int *err);
+void    asl_grad(    ASL *asl, double *x, double *g, int *err);
+void    asl_cons(    ASL *asl, double *x, double *c, int *err);
+double  asl_jcon(    ASL *asl, double *x, int j, int *err);
+void    asl_jcongrad(ASL *asl, double *x, double *g, int j, int *err);
 void    asl_hprod(   ASL *asl, double *y, double *v, double *hv, double w);
 void    asl_hvcompd( ASL *asl, double *v, double *hv, int nobj);
 void    asl_ghjvprod(ASL *asl, double *g, double *v, double *ghjv);
 
 size_t asl_sparse_congrad_nnz(ASL *asl, int j);
 void asl_sparse_congrad(
-    ASL *asl, double *x, int j, int64_t *inds, double *vals);
-void asl_jac( ASL *asl, double *x, int64_t *rows, int64_t *cols, double *vals);
+    ASL *asl, double *x, int j, int64_t *inds, double *vals, int *err);
+void asl_jac( ASL *asl, double *x, int64_t *rows, int64_t *cols, double *vals, int *err);
 void asl_hess(
     ASL *asl, double *y, double w, int64_t *rows, int64_t *cols, double *vals);
 
