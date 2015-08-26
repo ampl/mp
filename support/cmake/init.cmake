291fa70bb2bf88ade8b4d644656e2b575be2cfde
vitaut <victor.zverovich@gmail.com>
2015-08-25 17:26:56 -0700

    Rearrange stuff

--- support/cmake/init.cmake
+++ support/cmake/init.cmake
@@ -1,40 +1,5 @@
 # CMake initialization code that should be run before the project command.
 
-if (MP_WINSDK)
-  # Find Windows SDK.
-  set(winsdk_key
-    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows")
-  find_program(MP_SETENV NAMES SetEnv.cmd
-    PATHS "[${winsdk_key};CurrentInstallFolder]/bin")
-  if (MP_SETENV)
-    # Call SetEnv.cmd and set environment variables accordingly.
-    message(STATUS "Found SetEnv: ${MP_SETENV}")
-    file(WRITE CMakeFiles\\run-setenv.bat "call %*\nset\n")
-    execute_process(COMMAND CMakeFiles\\run-setenv.bat "${MP_SETENV}"
-      OUTPUT_VARIABLE out ERROR_VARIABLE err)
-    string(REPLACE ";" "\;" out "${out}")
-    string(REGEX MATCHALL "[^\n]+" out "${out}")
-    foreach (env ${out})
-      if (env MATCHES "([^=]+)=(.*)")
-        set(ENV{${CMAKE_MATCH_1}} "${CMAKE_MATCH_2}")
-      endif ()
-    endforeach ()
-
-    # If Microsoft SDK is installed create script run-msbuild.bat that
-    # calls SetEnv.cmd to set up build environment and runs msbuild.
-    # It is useful when building Visual Studio projects with the SDK
-    # toolchain rather than Visual Studio.
-    # Set FrameworkPathOverride to get rid of MSB3644 warnings.
-    if (NOT CMAKE_GENERATOR MATCHES Win64)
-      set(setenv_arg "/x86")
-    endif ()
-    file(WRITE "${CMAKE_BINARY_DIR}/run-msbuild.bat" "
-      call \"${MP_SETENV}\" ${setenv_arg}
-      msbuild -p:FrameworkPathOverride=^\"C:\\Program Files^
-\\Reference Assemblies\\Microsoft\\Framework\\.NETFramework\\v4.0^\" %*")
-  endif ()
-endif ()
-
 include(CMakeParseArguments)
 
 # Joins arguments and sets the result to <var>.
@@ -88,3 +53,38 @@ override(CMAKE_USER_MAKE_RULES_OVERRIDE
   ${CMAKE_CURRENT_LIST_DIR}/c_flag_overrides.cmake)
 override(CMAKE_USER_MAKE_RULES_OVERRIDE_CXX
   ${CMAKE_CURRENT_LIST_DIR}/cxx_flag_overrides.cmake)
+
+if (MP_WINSDK)
+  # Find Windows SDK.
+  set(winsdk_key
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows")
+  find_program(MP_SETENV NAMES SetEnv.cmd
+    PATHS "[${winsdk_key};CurrentInstallFolder]/bin")
+  if (MP_SETENV)
+    # Call SetEnv.cmd and set environment variables accordingly.
+    message(STATUS "Found SetEnv: ${MP_SETENV}")
+    file(WRITE CMakeFiles\\run-setenv.bat "call %*\nset\n")
+    execute_process(COMMAND CMakeFiles\\run-setenv.bat "${MP_SETENV}"
+      OUTPUT_VARIABLE out ERROR_VARIABLE err)
+    string(REPLACE ";" "\;" out "${out}")
+    string(REGEX MATCHALL "[^\n]+" out "${out}")
+    foreach (env ${out})
+      if (env MATCHES "([^=]+)=(.*)")
+        set(ENV{${CMAKE_MATCH_1}} "${CMAKE_MATCH_2}")
+      endif ()
+    endforeach ()
+
+    # If Microsoft SDK is installed create script run-msbuild.bat that
+    # calls SetEnv.cmd to set up build environment and runs msbuild.
+    # It is useful when building Visual Studio projects with the SDK
+    # toolchain rather than Visual Studio.
+    # Set FrameworkPathOverride to get rid of MSB3644 warnings.
+    if (NOT CMAKE_GENERATOR MATCHES Win64)
+      set(setenv_arg "/x86")
+    endif ()
+    file(WRITE "${CMAKE_BINARY_DIR}/run-msbuild.bat" "
+      call \"${MP_SETENV}\" ${setenv_arg}
+      msbuild -p:FrameworkPathOverride=^\"C:\\Program Files^
+\\Reference Assemblies\\Microsoft\\Framework\\.NETFramework\\v4.0^\" %*")
+  endif ()
+endif ()
