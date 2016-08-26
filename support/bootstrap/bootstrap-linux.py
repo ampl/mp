e4d730407c597e978e8353db7d91edc780df2187
Filipe Brandao <fdabrandao@gmail.com>
2016-08-26 12:20:25 -0400

    Generalise linux bootstrap scripts

--- support/bootstrap/bootstrap-linux.py
+++ support/bootstrap/bootstrap-linux.py
@@ -4,12 +4,12 @@ Set up build environment on Ubuntu or other Debian-based
 Linux distribution.
 
 Usage:
-  bootstrap-linux.py [buildbot | docker]
+  bootstrap-linux.py <version> <name> <mirror> [buildbot | docker]
 
 buildbot: install buildbot slave
 """
 
-import platform, re, shutil
+import platform, re, os, shutil
 from bootstrap import *
 from subprocess import check_call, Popen, PIPE
 
@@ -19,24 +19,34 @@ if __name__ == '__main__':
   import docopt
   args = docopt.docopt(__doc__)
 
+  version = args['<version>']
+  image_name = args['<name>']
+  mirror = args['<mirror>']
   x86_64 = platform.architecture()[0] == '64bit'
+  docker = args['docker']
 
+  env = os.environ.copy()
+  env['DEBIAN_FRONTEND'] = 'noninteractive'
+  
   # Install build tools.
   if not installed('cmake'):
-    # Install python-software-properties for apt-add-repository.
-    check_call(['apt-get', 'install', '-qy', 'python-software-properties'])
+    # Install python-software-properties or software-properties-common for apt-add-repository.
+    try:
+      check_call(['apt-get', 'install', '-qy', 'python-software-properties'], env=env)
+    except:
+      check_call(['apt-get', 'install', '-qy', 'software-properties-common'], env=env)
     # Add git-core PPA for newer version of Git because version 1.7 available
     # in Lucid cannot access private repos on GitHub via a token.
-    check_call(['add-apt-repository', 'ppa:git-core/ppa'])
+    check_call(['add-apt-repository', 'ppa:git-core/ppa'], env=env)
     # Add webupd8team java PPA for Java 7.
-    check_call(['add-apt-repository', 'ppa:webupd8team/java'])
+    check_call(['add-apt-repository', 'ppa:webupd8team/java'], env=env)
     # Suppress license dialog.
     cmd = 'echo debconf shared/accepted-oracle-license-v1-1 {0} true | ' + \
           'debconf-set-selections'
-    check_call(cmd.format('select'), shell=True)
-    check_call(cmd.format('seen'), shell=True)
+    check_call(cmd.format('select'), shell=True, env=env)
+    check_call(cmd.format('seen'), shell=True, env=env)
     # Install packages.
-    check_call(['apt-get', 'update', '-q'])
+    check_call(['apt-get', 'update', '-q'], env=env)
     packages = [
       'doxygen', 'git-core', 'gcc', 'g++', 'gfortran', 'ccache', 'make',
       'oracle-java7-installer', 'oracle-java7-set-default',
@@ -45,7 +55,7 @@ if __name__ == '__main__':
     ]
     if x86_64:
       packages.append('libc6-i386')
-    check_call(['apt-get', 'install', '-qy'] + packages)
+    check_call(['apt-get', 'install', '-qy'] + packages, env=env)
     shutil.rmtree('/var/cache/oracle-jdk7-installer')
 
     install_cmake('cmake-3.3.0-Linux-i386.tar.gz')
@@ -57,19 +67,20 @@ if __name__ == '__main__':
 
   install_f90cache()
   output = Popen(['gfortran', '--version'], stdout=PIPE).communicate()[0]
-  version = re.match(r'.* (\d+\.\d+)\.\d+', output).group(1)
-  add_to_path('/usr/local/bin/f90cache', 'gfortran-' + version)
+  gf_version = re.match(r'.* (\d+\.\d+)\.\d+', output).group(1)
+  if gf_version.split('.')[:2] <= [5, 2]:
+    # f90cache 0.96 is not compatible with gfortran > 5.2
+    add_to_path('/usr/local/bin/f90cache', 'gfortran-' + gf_version)
 
-  docker = args['docker']
   if docker:
     # Install xvfb and miwm (a window manager) for GUI tests and x11vnc to
     # be able to remotely connect to the X server for debugging.
     check_call(['add-apt-repository',
-                'deb http://archive.ubuntu.com/ubuntu lucid universe'])
-    check_call(['apt-get', 'update', '-q'])
-    check_call(['apt-get', 'install', '-qy', 'xvfb', 'x11vnc', 'miwm'])
+                'deb {0} {1} universe'.format(mirror, version)], env=env)
+    check_call(['apt-get', 'update', '-q'], env=env)
+    check_call(['apt-get', 'install', '-qy', 'xvfb', 'x11vnc', 'miwm'], env=env)
 
-  check_call(['apt-get', 'clean'])
+  check_call(['apt-get', 'clean'], env=env)
 
   # Install LocalSolver.
   if not installed('localsolver'):
@@ -82,11 +93,11 @@ if __name__ == '__main__':
 
   if args['buildbot'] or docker:
     ip = '172.17.42.1' if docker else None
-    path = install_buildbot_slave(
-      'lucid64' if x86_64 else 'lucid32', ip=ip)
+    path = install_buildbot_slave(image_name, ip=ip)
     if not docker and path:
       pip_install('python-crontab', 'crontab')
       from crontab import CronTab
+      username = 'buildbot'
       cron = CronTab(username)
       cron.new('PATH={0}:/usr/local/bin buildslave start {1}'.format(
         os.environ['PATH'], path)).every_reboot()
