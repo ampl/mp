b72cac6d2d441dae25473cfbe1389c4de0cfa822
Filipe Brandao <fdabrandao@gmail.com>
2016-09-28 19:16:41 -0400

    Add authentication to buildbot

--- support/buildbot/update-config.sh
+++ support/buildbot/update-config.sh
@@ -1,8 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 # This script uploads buildbot config to the server and reconfigs
 # the buildbot.
+BASEDIR=`dirname $0`
+cd $BASEDIR
+
+if [ $# -ne 1 ]; then
+    echo -e "Usage:"
+    echo -e "  $0 <password>"
+    exit 1
+else
+    PASSWORD=$1
+fi
 
 BUILDBOT_DIR=`ssh ampl.com "echo ~buildbot"`
-scp master.cfg ampl.com:$BUILDBOT_DIR/master.cfg
+cp master.cfg tmp.cfg
+sed -r 's/\[\("admin","password"\)\]/\[\("admin","'$PASSWORD'"\)\]/g' \
+    master.cfg > tmp.cfg
+scp tmp.cfg ampl.com:$BUILDBOT_DIR/master.cfg
 scp templates/layout.html ampl.com:$BUILDBOT_DIR/templates/layout.html
 ssh -t ampl.com "cd $BUILDBOT_DIR; sudo -H -u buildbot buildbot reconfig"
+rm tmp.cfg
