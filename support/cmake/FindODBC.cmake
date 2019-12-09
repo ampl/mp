5d7bec9a293490e642b077d767b41c692f896554
Christian Valente <christian.valente@outlook.com>
2019-12-09 12:00:14 +0200

    Updated build scripts for 64 bits build on windows

--- support/cmake/FindODBC.cmake
+++ support/cmake/FindODBC.cmake
@@ -6,18 +6,34 @@
 #  ODBC_INCLUDE_DIRS - The ODBC include directories
 #  ODBC_LIBRARIES - The libraries needed to use ODBC
 
-set(winsdk_key
-  "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows")
-find_path(ODBC_INCLUDE_DIR sql.h
-  PATHS "[${winsdk_key};CurrentInstallFolder]/include")
-
-set(libdir_suffix )
-if (CMAKE_SIZEOF_VOID_P EQUAL 8)
-  set(libdir_suffix x64)
-endif ()
-
 find_library(ODBC_LIBRARY NAMES odbc odbc32
   PATHS "[${winsdk_key};CurrentInstallFolder]/lib/${libdir_suffix}")
+
+  if(WIN32)
+    unset(ODBC_LIBRARY CACHE)
+    find_package(WindowsSDK)
+    GetUMWindowsSKDLibraryDir(WIN_LIBRARY_DIR)
+    GetUMWindowsSKDIncludeDir(WIN_INCLUDE_DIR)
+    find_path(ODBC_INCLUDE_DIR sql.h
+      PATHS "${WIN_INCLUDE_DIR}" NO_DEFAULT_PATH)
+    find_library(ODBC_LIBRARY NAMES odbc odbc32
+      PATHS "${WIN_LIBRARY_DIR}" NO_DEFAULT_PATH) 
+else()
+  # keeping old method on non-windows, can probably be removed altogether
+  set(winsdk_key
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows")
+  find_path(ODBC_INCLUDE_DIR sql.h
+    PATHS "[${winsdk_key};CurrentInstallFolder]/include")
+  set(libdir_suffix )
+  if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(libdir_suffix x64)
+  endif ()
+  find_library(ODBC_LIBRARY NAMES odbc odbc32
+    PATHS "[${winsdk_key};CurrentInstallFolder]/lib/${libdir_suffix}")
+endif()
+
+
+
 
 if (APPLE)
   find_library(COREFOUNDATION_LIBRARY CoreFoundation)
