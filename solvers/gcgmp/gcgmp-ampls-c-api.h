a0abcd246cecd5db3a07d1b47e300671fc1aca7d
Jurgen Lentz <jurgen.lentz@rwth-aachen.de>
2023-06-01 12:49:35 +0200

    scip and gcg update

--- solvers/gcgmp/gcgmp-ampls-c-api.h
+++ solvers/gcgmp/gcgmp-ampls-c-api.h
@@ -1,32 +1,27 @@
 #ifndef GCGAMPLSCAPI_H
 #define GCGAMPLSCAPI_H
 /*
- * C API for MP/Gcg
- */
+  * C API for MP/Gcg
+  */
 
-#include "gcg/gcg.h"
+ //#include "gcg.h"
 
-#include "mp/ampls-c-api.h"
-
-/*
- * Below are Gcg-specific AMPLS API functions.
- * They complement the 'public' AMPLS API defined in ampls-c-api.h.
- */
-
-/// Initialize AMPLS gcg.
-/// @param slv_opt: a string of solver options
-/// (normally provided in the <solver>_options string).
-/// Can be NULL.
-/// @return 0 on success, otherwise see ret_val->warnings_and_or_errors_
-AMPLS_MP_Solver* Open_gcg(const char* slv_opt, CCallbacks cb);
-
-/// Shut down solver instance
-AMPLS_C_EXPORT void AMPLSClose_gcg(AMPLS_MP_Solver* slv);
-
-/// Extract the Gurobi model handle
-AMPLS_C_EXPORT SCIP* AMPLSGetModel_gcg(AMPLS_MP_Solver* slv);
+ #include "mp/ampls-c-api.h"
 
 
-#endif // GCGAMPLSCAPI_H
+ /// Initialize AMPLS gcg.
+
+ /// @param slv_opt: a string of solver options
+ /// (normally provided in the <solver>_options string).
+ /// Can be NULL.
+ /// @return pointer to struct AMPLS_MP_Solver to be populated.
+ void*  AMPLSOpenGcg(const char* slv_opt, CCallbacks cb);
+
+ /// Shut down solver instance
+ void AMPLSCloseGcg(AMPLS_MP_Solver* slv);
+
+ /// Extract the Gcg model handle
+ void* GetGcgmodel(AMPLS_MP_Solver* slv);
 
 
+ #endif // GCGAMPLSCAPI_H
--- solvers/scipmp/scipmp-ampls-c-api.h
+++ solvers/scipmp/scipmp-ampls-c-api.h
@@ -1,32 +1,27 @@
 #ifndef SCIPAMPLSCAPI_H
 #define SCIPAMPLSCAPI_H
 /*
- * C API for MP/Scip
- */
+  * C API for MP/Scip
+  */
 
-#include "scip/scip.h"
+ //#include "scip.h"
 
-#include "mp/ampls-c-api.h"
-
-/*
- * Below are Scip-specific AMPLS API functions.
- * They complement the 'public' AMPLS API defined in ampls-c-api.h.
- */
-
-/// Initialize AMPLS scip.
-/// @param slv_opt: a string of solver options
-/// (normally provided in the <solver>_options string).
-/// Can be NULL.
-/// @return 0 on success, otherwise see ret_val->warnings_and_or_errors_
-AMPLS_MP_Solver* Open_scip(const char* slv_opt, CCallbacks cb);
-
-/// Shut down solver instance
-AMPLS_C_EXPORT void AMPLSClose_scip(AMPLS_MP_Solver* slv);
-
-/// Extract the Gurobi model handle
-AMPLS_C_EXPORT SCIP* AMPLSGetModel_scip(AMPLS_MP_Solver* slv);
+ #include "mp/ampls-c-api.h"
 
 
-#endif // SCIPAMPLSCAPI_H
+ /// Initialize AMPLS scip.
+
+ /// @param slv_opt: a string of solver options
+ /// (normally provided in the <solver>_options string).
+ /// Can be NULL.
+ /// @return pointer to struct AMPLS_MP_Solver to be populated.
+ void*  AMPLSOpenScip(const char* slv_opt, CCallbacks cb);
+
+ /// Shut down solver instance
+ void AMPLSCloseScip(AMPLS_MP_Solver* slv);
+
+ /// Extract the Scip model handle
+ void* GetScipmodel(AMPLS_MP_Solver* slv);
 
 
+ #endif // SCIPAMPLSCAPI_H
