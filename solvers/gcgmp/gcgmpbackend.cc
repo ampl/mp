6a383b17aa65a34bae0c18281fa2211de63af201
Christian Valente <christian.valente@outlook.com>
2023-06-07 22:26:54 +0200

    Fixed name of API function in gcg

--- solvers/gcgmp/gcgmpbackend.cc
+++ solvers/gcgmp/gcgmpbackend.cc
@@ -1079,7 +1079,7 @@ void GcgBackend::AddMIPStart(ArrayRef<double> x0, ArrayRef<int> sparsity) {
 
 
 // AMPLs
-void* AMPLSOpen_gcg(CCallbacks cb = {}) {
+void* Open_gcg(CCallbacks cb = {}) {
   return AMPLS__internal__Open(std::unique_ptr<mp::BasicBackend>{new mp::GcgBackend()},
     cb);
 }
