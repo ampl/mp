349129c78d42b603e4e022d0536e302dc10f3ac5
Christian Valente <christian.valente@outlook.com>
2023-06-27 19:52:39 +0200

    Added second entry point to SCIP main function for compiling with CPLEX

--- solvers/scipmp/main.cc
+++ solvers/scipmp/main.cc
@@ -1,9 +1,11 @@
 #include "mp/backend-app.h"
 
-/// Declare a backend factory
 std::unique_ptr<mp::BasicBackend> CreateScipBackend();
 
-int main(int, char **argv) {
-  return
-      mp::RunBackendApp(argv, CreateScipBackend);
+extern "C" int main1(int, char** argv) {
+  return mp::RunBackendApp(argv, CreateScipBackend);
+}
+
+extern "C" int main2(int, char** argv, CCallbacks cb) {
+  return mp::RunBackendApp(argv, CreateScipBackend, cb);
 }
