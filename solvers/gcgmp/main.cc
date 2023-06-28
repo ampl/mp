c61df066d72801875ff70c54509ba0fdaa89e70c
Jurgen Lentz <jurgen.lentz@rwth-aachen.de>
2023-06-29 01:04:33 +0200

    add main2 for GCG CPLEX

--- solvers/gcgmp/main.cc
+++ solvers/gcgmp/main.cc
@@ -1,9 +1,11 @@
 #include "mp/backend-app.h"
 
-/// Declare a backend factory
 std::unique_ptr<mp::BasicBackend> CreateGcgBackend();
 
-extern "C" int main(int, char **argv) {
-  return
-      mp::RunBackendApp(argv, CreateGcgBackend);
+extern "C" int main1(int, char** argv) {
+  return mp::RunBackendApp(argv, CreateGcgBackend);
+}
+
+extern "C" int main2(int, char** argv, CCallbacks cb) {
+  return mp::RunBackendApp(argv, CreateGcgBackend, cb);
 }
