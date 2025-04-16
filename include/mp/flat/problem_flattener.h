29456b64f3275d2db57619b02bc3ca2007ed4422
Gleb Belov <gleb@ampl.com>
2025-04-16 14:29:41 +1000

    Fix cvt:multoutcard
    Inline QP terms for objectives: do return expression

--- include/mp/flat/problem_flattener.h
+++ include/mp/flat/problem_flattener.h
@@ -1210,11 +1210,10 @@ public:         // More utilities
     auto resvar1 = Convert2Var( std::move(el) );
     auto resvar2 = (&el==&er || el==er)
                        ? resvar1 : Convert2Var( std::move(er) );
-    if (ifFltCon_)
-      return {                         // = el*er+0, inlined  #260
-              LinTerms{},
-              QuadTerms{ {1.0}, {resvar1}, {resvar2} },
-              0.0 };
+    return {                         // = el*er+0, inlined  #260
+            LinTerms{},
+            QuadTerms{ {1.0}, {resvar1}, {resvar2} },
+            0.0 };
   }
 
   /// Multiply out two EEXprs
