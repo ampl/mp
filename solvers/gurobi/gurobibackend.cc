4e9060c25456ba7703788687e491f78eb8afa90f
Gleb Belov <gleb@ampl.com>
2026-02-12 17:54:08 +1100

    Gurobi alg:method: PDHG: mention alg:pdhggpu

--- solvers/gurobi/gurobibackend.cc
+++ solvers/gurobi/gurobibackend.cc
@@ -1503,7 +1503,8 @@ static const mp::OptionValueInfo values_method[] = {
   { "3", "Nondeterministic concurrent (several solves in parallel)", 3},
   { "4", "Deterministic concurrent", 4},
   { "5", "Deterministic concurrent simplex (deprecated; use alg:concurrentmethod).", 5},
-  {"6", "PDHG (Primal-Dual Hybrid Gradient)", 6}
+  {"6", "PDHG (Primal-Dual Hybrid Gradient). "
+           "For PDHG on GPU, see alg:pdhggpu.", 6}
 };
 static const mp::OptionValueInfo values_concurrentmethod[] = {
    { "-1", "Automatic (default)", -1},
