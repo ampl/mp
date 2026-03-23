f704e48d8380aa7eeda68f7a75899ac0d254fcc2
Christian Valente <christian.valente@outlook.com>
2026-03-23 10:37:27 +0200

    Copt: set expression support disabled by default

--- solvers/copt/coptmodelapi.h
+++ solvers/copt/coptmodelapi.h
@@ -144,7 +144,7 @@ public:
   //////////////////////////// EXPRESSION TREES ////////////////////////////
   /// Handle expression trees: inherit basic API
   USE_BASE_EXPRESSION_HANDLERS(BaseModelAPI)
-  ACCEPT_EXPRESSION_INTERFACE(Recommended)
+  ACCEPT_EXPRESSION_INTERFACE(AcceptedButNotRecommended)
 
 
 
