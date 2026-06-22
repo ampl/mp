1acd6afe13fbb3a171056513d7de4f57d420162c
Gleb Belov <gleb@ampl.com>
2026-06-22 11:02:05 +1000

    MP_LINK_WITH_SHARED_LIB: prototype for main()

--- solvers/highsmp/highsmp-lib.c
+++ solvers/highsmp/highsmp-lib.c
@@ -8,8 +8,10 @@ AMPLS_C_EXPORT AMPLS_MP_Solver* AMPLSOpen_highs(int argc, char** argv)
 }
 
 
-
 #ifdef MP_LINK_WITH_SHARED_LIB
+
+int main(int argc, char** argv);
+
 AMPLS_C_EXPORT int highs_main(int argc, char** argv)
 {
 	return main(argc, argv);
--- solvers/mp2nl/mp2nl-lib.c
+++ solvers/mp2nl/mp2nl-lib.c
@@ -8,6 +8,9 @@ AMPLS_C_EXPORT AMPLS_MP_Solver* AMPLSOpen_mp2nl(int argc, char** argv)
 
 
 #ifdef MP_LINK_WITH_SHARED_LIB
+
+int main(int argc, char** argv);
+
 AMPLS_C_EXPORT int mp2nl_main(int argc, char** argv)
 {
 	return main(argc, argv);
